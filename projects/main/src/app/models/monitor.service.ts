57484863c1f5c5dbbfb9168f4fa1ca2b965f75ca
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-03-18 16:38:36 +0900

    fix: 不要なmodelsの削除など

--- projects/main/src/app/models/monitor.service.ts
+++ projects/main/src/app/models/monitor.service.ts
@@ -1,5 +1,4 @@
 import { ConfigService } from './config.service';
-import { GentxResponse } from './cosmos/gentx.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
@@ -25,20 +24,5 @@ export class MonitorService {
         count: count,
       },
     });
-  }
-
-  postGentxStringToSlack$(gentxString: string): Observable<GentxResponse> {
-    const requestBody = {
-      gentx_string: gentxString,
-    };
-    const requestUrl = `${this.config.config.extension?.monitor?.monitorURL}/gentx`;
-    if (this.config.config.extension?.monitor?.monitorURL !== undefined) {
-      return this.http.post<GentxResponse>(requestUrl, requestBody);
-    } else {
-      return of({
-        status: false,
-        message: 'requestUrl is undefined!',
-      });
-    }
   }
 }

0b44b35571b1154934fc99c894050e0b7418e18f
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-03-23 14:53:35 +0900

    feat: move monitor to other repository

--- projects/monitor/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/monitor
-docker-compose down
-docker pull ghcr.io/cauchye/telescope-monitor:latest
-# curl -L https://raw.githubusercontent.com/CauchyE/telescope/main/projects/monitor/config.json -o config.json
-# vi config.json
-curl -O https://raw.githubusercontent.com/CauchyE/telescope/main/projects/monitor/docker-compose.yml
-docker-compose up -d

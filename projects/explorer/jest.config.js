1b7b38c99edf555fa7dff624ddd5b4c88188e8a7
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-03-24 20:33:31 +0900

    fix: test related path settings

--- projects/explorer/jest.config.js
+++ projects/explorer/jest.config.js
@@ -2,6 +2,6 @@ require('jest-preset-angular/ngcc-jest-processor');
 
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/projects/main/src/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/projects/main/cypress/']
+  setupFilesAfterEnv: ['<rootDir>/projects/explorer/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/projects/explorer/cypress/']
 };

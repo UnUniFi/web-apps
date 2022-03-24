b258b5e31b70093c1447e0ffbafd2ba76d7db69b
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-03-24 10:38:55 +0900

    fix: rename directory from main to explorer

---
+++ projects/explorer/src/app/pages/home/distribution/distribution.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { rest } from '@cosmos-client/core';
+import { CosmosDistributionV1beta1QueryCommunityPoolResponse } from '@cosmos-client/core/esm/openapi/api';
+import { CosmosSDKService } from 'projects/explorer/src/app/models/cosmos-sdk.service';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-distribution',
+  templateUrl: './distribution.component.html',
+  styleUrls: ['./distribution.component.css'],
+})
+export class DistributionComponent implements OnInit {
+  communityPool$: Observable<CosmosDistributionV1beta1QueryCommunityPoolResponse>;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly cosmosSDK: CosmosSDKService,
+  ) {
+    this.communityPool$ = this.cosmosSDK.sdk$.pipe(
+      mergeMap((sdk) => rest.distribution.communityPool(sdk.rest).then((res) => res.data)),
+    );
+  }
+
+  ngOnInit(): void {}
+}
---
+++ projects/explorer/src/app/views/cosmos/staking/validators/validators.module.ts
@@ -0,0 +1,12 @@
+import { MaterialModule } from '../../../material.module';
+import { ValidatorsComponent } from './validators.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [ValidatorsComponent],
+  imports: [CommonModule, RouterModule, MaterialModule],
+  exports: [ValidatorsComponent],
+})
+export class ValidatorsModule {}

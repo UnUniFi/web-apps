e0b5da9d12f8c312e94ec320abc5fcffe570c04f
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-03-28 19:46:47 +0900

    fix: main to landing page

--- projects/landing-page/jest.config.js
+++ projects/landing-page/jest.config.js
@@ -2,6 +2,6 @@ require('jest-preset-angular/ngcc-jest-processor');
 
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/projects/main/src/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/projects/main/cypress/'],
+  setupFilesAfterEnv: ['<rootDir>/projects/landing-page/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/projects/landing-page/cypress/'],
 };

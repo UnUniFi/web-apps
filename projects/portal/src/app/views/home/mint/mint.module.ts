79a8d0b97a1dba47407c1b6d2069d1b7947ea8cb
taiki-furu <taiki.furusyo@gmail.com>
2022-04-01 17:45:18 +0900

    fix: for #310 PR in telescope

--- projects/portal/src/app/views/home/mint/mint.module.ts
+++ projects/portal/src/app/views/home/mint/mint.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MintComponent } from './mint.component';
-import { RouterModule } from '@angular/router';
+import { PipesModule } from '../../../pipes/pipes.module';
 import { MaterialModule } from '../../material.module';
+import { MintComponent } from './mint.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    MintComponent
-  ],
-  imports: [
-    CommonModule, RouterModule, MaterialModule
-  ],
-  exports: [
-    MintComponent
-  ],
+  declarations: [MintComponent],
+  imports: [CommonModule, RouterModule, MaterialModule, PipesModule],
+  exports: [MintComponent],
 })
-export class MintModule { }
+export class MintModule {}

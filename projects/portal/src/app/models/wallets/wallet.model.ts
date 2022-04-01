77f39ab840b590b5470d9438f3df5d8e93540f1f
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-02 08:16:29 +0900

    feat: wallet service

---
+++ projects/portal/src/app/models/wallets/wallet.model.ts
@@ -0,0 +1,38 @@
+import { KeyType } from '../keys/key.model';
+import { cosmosclient } from '@cosmos-client/core';
+import { PubKey } from '@cosmos-client/core/esm/types';
+
+// Note: Now, only ununifi is supported.
+export enum WalletType {
+  ununifi = 'ununifi',
+  kepler = 'kepler',
+  ledger = 'ledger',
+  keyStation = 'keyStation',
+}
+
+// Note: For general purpose? Uint8Array
+export type Wallet = {
+  id: string;
+  type: WalletType;
+  key_type: KeyType;
+  public_key: Uint8Array;
+  address: Uint8Array;
+};
+
+// Note: For Indexed DB (string)
+export type StoredWallet = {
+  id: string;
+  type: WalletType;
+  key_type: KeyType;
+  public_key: string;
+  address: string;
+};
+
+// Note: cosmos-client/core type
+export type CosmosWallet = {
+  id: string;
+  type: WalletType;
+  key_type: KeyType;
+  public_key: PubKey;
+  address: cosmosclient.AccAddress;
+};

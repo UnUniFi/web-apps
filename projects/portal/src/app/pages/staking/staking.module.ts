239d3d86bb44c281be760f2cb0ba3bb98896be61
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-08 21:30:10 +0900

    fix: module path

--- projects/portal/src/app/pages/staking/staking.module.ts
+++ projects/portal/src/app/pages/staking/staking.module.ts
@@ -1,5 +1,5 @@
-import { CreateValidatorModule } from '../../views/cosmos/staking/create-validator/create-validator.module';
 import { StakingModule } from '../../views/cosmos/staking/staking.module';
+import { CreateValidatorModule } from '../../views/staking/create-validator/create-validator.module';
 import { CreateValidatorComponent } from './create-validator/create-validator.component';
 import { StakingRoutingModule } from './staking-routing.module';
 import { CommonModule } from '@angular/common';

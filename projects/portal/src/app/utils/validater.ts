4e1a9b4d9af383cb9a46933fa4a9db3ed2eba019
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-08 20:02:22 +0900

    refactor: wallet application service and related utils

---
+++ projects/portal/src/app/utils/validater.ts
@@ -0,0 +1,34 @@
+import { StoredWallet } from '../models/wallets/wallet.model';
+import { createCosmosPrivateKeyFromString, createCosmosPublicKeyFromString } from './key';
+import { cosmosclient } from '@cosmos-client/core';
+
+export const validatePrivateStoredWallet = (
+  privateStoredWallet: StoredWallet & { privateKey: string },
+): boolean => {
+  const cosmosPrivateKey = createCosmosPrivateKeyFromString(
+    privateStoredWallet.key_type,
+    privateStoredWallet.privateKey,
+  );
+  if (!cosmosPrivateKey) {
+    return false;
+  }
+  const cosmosPublicKey = createCosmosPublicKeyFromString(
+    privateStoredWallet.key_type,
+    privateStoredWallet.public_key,
+  );
+  if (!cosmosPublicKey) {
+    return false;
+  }
+  const matchPrivateKeyAndPublicKey =
+    cosmosPrivateKey.pubKey().bytes().toString() !== cosmosPublicKey.bytes().toString();
+  if (matchPrivateKeyAndPublicKey) {
+    return false;
+  }
+  const matchPublicKeyAndAddress =
+    cosmosclient.AccAddress.fromPublicKey(cosmosPublicKey).toAccAddress().toString() !==
+    privateStoredWallet.address;
+  if (matchPublicKeyAndAddress) {
+    return false;
+  }
+  return true;
+};

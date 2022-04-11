c4d77cce56e439f059b1a1815850c837c429fe07
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-11 19:39:14 +0900

    chore: del old file

--- editions/ununifi/launch/ununifi-8-private-test/config/standard/deploy-explorer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/explorer
-docker-compose down
-docker pull ghcr.io/ununifi/explorer:test
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/docker/explorer/docker-compose.yml
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/projects/explorer/nginx.conf
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/config/standard/config.js
-docker-compose up -d
--- editions/ununifi/launch/ununifi-8-private-test/config/standard/deploy-portal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/portal
-docker-compose down
-docker pull ghcr.io/ununifi/portal:test
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/docker/portal/docker-compose.yml
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/projects/portal/nginx.conf
-curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/config/standard/config.js
-docker-compose up -d

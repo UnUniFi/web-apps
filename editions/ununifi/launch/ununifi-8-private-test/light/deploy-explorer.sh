caa18bfb8f7a83bbcbda5f56fd328b45d5918fbe
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-04-11 14:20:02 +0900

    feat: move editions(config) to root directory

---
+++ editions/ununifi/launch/ununifi-8-private-test/light/deploy-explorer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/explorer
+docker-compose down
+docker pull ghcr.io/ununifi/explorer:test
+curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/docker-compose.yml
+curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/projects/explorer/nginx.conf
+curl -O https://raw.githubusercontent.com/UnUniFi/utils/develop/editions/ununifi/launch/ununifi-8-private-test/light/config.js
+docker-compose up -d

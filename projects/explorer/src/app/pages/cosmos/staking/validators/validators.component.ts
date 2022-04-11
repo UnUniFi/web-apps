704f7455d661a5c6a36cbf52e5c8ccd5a906690a
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-04-11 23:11:16 +0900

    feat: set layout / add active, inactive button

--- projects/explorer/src/app/pages/cosmos/staking/validators/validators.component.ts
+++ projects/explorer/src/app/pages/cosmos/staking/validators/validators.component.ts
@@ -1,25 +1,13 @@
 import { CosmosSDKService } from '../../../../models/cosmos-sdk.service';
-import { Component, OnInit } from '@angular/core';
-import { docData } from '@angular/fire/firestore';
-import { rest } from '@cosmos-client/core';
 import {
-  QueryValidatorsResponseIsResponseTypeForTheQueryValidatorsRPCMethod,
-  InlineResponse20066Validators,
-} from '@cosmos-client/core/esm/openapi';
-import { Observable, from, BehaviorSubject } from 'rxjs';
-import { map, mergeMap, withLatestFrom, filter } from 'rxjs/operators';
-
-export type validatorType = {
-  val: InlineResponse20066Validators;
-  share: number;
-  isActive: boolean;
-  rank: number;
-};
-
-export type validatorWithShareType = {
-  val: InlineResponse20066Validators;
-  share: number;
-};
+  validatorType,
+  validatorWithShareType,
+} from './../../../../views/cosmos/staking/validators/validators.component';
+import { Component, OnInit } from '@angular/core';
+import { rest } from '@cosmos-client/core';
+import { QueryValidatorsResponseIsResponseTypeForTheQueryValidatorsRPCMethod } from '@cosmos-client/core/esm/openapi';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { map, mergeMap, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-validators',
@@ -29,20 +17,16 @@ export type validatorWithShareType = {
 export class ValidatorsComponent implements OnInit {
   validatorsList$: Observable<QueryValidatorsResponseIsResponseTypeForTheQueryValidatorsRPCMethod>;
   allValidatorsTokens$: Observable<number | undefined>;
-
   validatorsWithShare$: Observable<validatorWithShareType[]>;
-
   validators$: Observable<validatorType[]>;
 
-  activeEnabled: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  activeEnabled: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   constructor(private cosmosSDK: CosmosSDKService) {
     this.validatorsList$ = this.cosmosSDK.sdk$.pipe(
       mergeMap((sdk) => rest.staking.validators(sdk.rest)),
       map((result) => result.data),
     );
-    //for debug
-    this.validatorsList$.subscribe((x) => console.log('val', x));
 
     this.allValidatorsTokens$ = this.validatorsList$.pipe(
       map((validators) =>
@@ -64,37 +48,42 @@ export class ValidatorsComponent implements OnInit {
           const share = Number(validator.tokens) / allTokens;
           return { val, share };
         });
-        const validatorsWithSort = validatorsWithShare?.sort((x, y) => x.share - y.share);
+        // sort by share
+        const validatorsWithSort = validatorsWithShare?.sort((x, y) => y.share - x.share);
+
         return validatorsWithSort || [];
       }),
     );
 
-    this.validatorsWithShare$.subscribe((x) => console.log('val2', x));
-
-    this.validators$ = this.validatorsWithShare$.pipe(
-      map((x) => {
-        const cc = x
-          .map((x, index) => {
-            // calculate validator share
-            const val = x.val;
-            const share = x.share;
-            const rank = index + 1;
-            const statusBonded = x.val.status === 'BOND_STATUS_BONDED';
-            const isActive = statusBonded && rank < 50;
-            return { val, share, isActive, rank };
+    // select listing validators by checking status and rank
+    this.validators$ = combineLatest([this.validatorsWithShare$, this.activeEnabled]).pipe(
+      map(([validatorWithShare, activeEnabled]) =>
+        validatorWithShare
+          .map((validatorWithShare, index) => {
+            if (activeEnabled) {
+              const val = validatorWithShare.val;
+              const share = validatorWithShare.share;
+              const rank = index + 1;
+              const statusBonded = validatorWithShare.val.status === 'BOND_STATUS_BONDED';
+              const inList = statusBonded && rank <= 50;
+              return { val, share, inList, rank };
+            } else {
+              const val = validatorWithShare.val;
+              const share = validatorWithShare.share;
+              const rank = index + 1;
+              const statusBonded = validatorWithShare.val.status === 'BOND_STATUS_BONDED';
+              const inList = !(statusBonded && rank <= 50);
+              return { val, share, inList, rank };
+            }
           })
-          .filter((x) => x.isActive);
-
-        return cc;
-      }),
+          .filter((validator) => validator.inList),
+      ),
     );
-    this.validators$.subscribe((x) => console.log('val3', x));
-    //this.validatorShares$.subscribe((x) => console.log('s', x));
   }
 
   ngOnInit() {}
 
-  onCheckBoxAutoChange(checked: boolean) {
-    this.activeEnabled.next(checked);
+  onToggleChange(value: boolean) {
+    this.activeEnabled.next(value);
   }
 }
--- projects/explorer/src/app/views/cosmos/staking/validators/validators.component.ts
+++ projects/explorer/src/app/views/cosmos/staking/validators/validators.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {
-  InlineResponse20066Validators,
-  QueryValidatorsResponseIsResponseTypeForTheQueryValidatorsRPCMethod,
-} from '@cosmos-client/core/esm/openapi';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { InlineResponse20066Validators } from '@cosmos-client/core/esm/openapi';
 import * as crypto from 'crypto';
-import {
-  validatorType,
-  validatorWithShareType,
-} from 'projects/explorer/src/app/pages/cosmos/staking/validators/validators.component';
+
+export type validatorType = {
+  val: InlineResponse20066Validators;
+  share: number;
+  inList: boolean;
+  rank: number;
+};
+
+export type validatorWithShareType = {
+  val: InlineResponse20066Validators;
+  share: number;
+};
 
 @Component({
   selector: 'view-validators',
@@ -18,7 +23,12 @@ export class ValidatorsComponent implements OnInit {
   @Input()
   validators?: validatorType[] | null;
 
-  constructor() {}
+  @Output()
+  toggleActiveChange: EventEmitter<boolean>;
+
+  constructor() {
+    this.toggleActiveChange = new EventEmitter();
+  }
 
   ngOnInit(): void {
     setTimeout(() => {
@@ -34,5 +44,14 @@ export class ValidatorsComponent implements OnInit {
       .toString('hex');
 
     return `#${hash.substr(0, 6)}`;
+  }
+
+  onToggleChange(value: string) {
+    if (value == 'active') {
+      this.toggleActiveChange.emit(true);
+    }
+    if (value == 'inactive') {
+      this.toggleActiveChange.emit(false);
+    }
   }
 }

7bc7c223c89473ff7d46242e872d1c6550bbf7d0
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-11 16:17:41 +0900

    chore: del line

--- projects/portal/src/app/models/faucets/faucet.infrastructure.service.ts
+++ projects/portal/src/app/models/faucets/faucet.infrastructure.service.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ConfigService } from 'projects/portal/src/app/models/config.service';
 
-
 @Injectable({
   providedIn: 'root',
 })

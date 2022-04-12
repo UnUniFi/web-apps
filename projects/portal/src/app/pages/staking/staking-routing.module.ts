3caac23a939d9894e2f10ae1dd069dccf1bd0ea0
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-04-12 13:03:38 +0900

    fix: change routing

--- projects/portal/src/app/pages/staking/staking-routing.module.ts
+++ projects/portal/src/app/pages/staking/staking-routing.module.ts
@@ -1,9 +1,15 @@
 import { WalletGuard } from '../../models/wallets/wallet.guard';
 import { CreateValidatorComponent } from './create-validator/create-validator.component';
+import { StakingComponent } from './staking.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: StakingComponent,
+    canActivate: [WalletGuard],
+  },
   {
     path: 'create-validator',
     component: CreateValidatorComponent,

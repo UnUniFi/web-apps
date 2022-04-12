7aee3067b543644eb4887a426e1b2729ae89ebd6
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-12 14:43:34 +0900

    chore: import

--- projects/portal/src/app/pages/staking/staking.module.ts
+++ projects/portal/src/app/pages/staking/staking.module.ts
@@ -1,4 +1,5 @@
 import { CreateValidatorModule } from '../../views/staking/create-validator/create-validator.module';
+import { StakingModule } from '../../views/staking/staking.module';
 import { CreateValidatorComponent } from './create-validator/create-validator.component';
 import { StakingRoutingModule } from './staking-routing.module';
 import { CommonModule } from '@angular/common';
@@ -6,6 +7,6 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [CreateValidatorComponent],
-  imports: [CommonModule, StakingRoutingModule, CreateValidatorModule],
+  imports: [CommonModule, StakingRoutingModule, CreateValidatorModule, StakingModule],
 })
 export class AppStakingModule {}

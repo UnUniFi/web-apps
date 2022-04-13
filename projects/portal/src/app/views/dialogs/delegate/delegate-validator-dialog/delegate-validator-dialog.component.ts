eedbc4654446ab9789186a5a88e4913b05a40d81
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-13 17:30:02 +0900

    feat: delegate dialog view

--- projects/portal/src/app/views/dialogs/delegate/delegate-validator-dialog/delegate-validator-dialog.component.ts
+++ projects/portal/src/app/views/dialogs/delegate/delegate-validator-dialog/delegate-validator-dialog.component.ts
@@ -1,9 +1,13 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { proto } from '@cosmos-client/core';
 import { InlineResponse20066Validators } from '@cosmos-client/core/esm/openapi';
 import * as crypto from 'crypto';
 import { StoredWallet } from 'projects/portal/src/app/models/wallets/wallet.model';
+
+export type DelegateOnSubmitEvent = {
+  amount: proto.cosmos.base.v1beta1.ICoin;
+  minimumGasPrice: proto.cosmos.base.v1beta1.ICoin;
+};
 
 @Component({
   selector: 'view-delegate-validator-dialog',
@@ -11,12 +15,35 @@ import { StoredWallet } from 'projects/portal/src/app/models/wallets/wallet.mode
   styleUrls: ['./delegate-validator-dialog.component.css'],
 })
 export class DelegateValidatorDialogComponent implements OnInit {
-  constructor(
-    @Inject(MAT_DIALOG_DATA)
-    public readonly data: StoredWallet & InlineResponse20066Validators,
-    public matDialogRef: MatDialogRef<DelegateValidatorDialogComponent>,
-    private readonly snackBar: MatSnackBar,
-  ) {}
+  @Input()
+  currentStoredWallet?: StoredWallet | null;
+  @Input()
+  coins?: proto.cosmos.base.v1beta1.ICoin[] | null;
+  @Input()
+  minimumGasPrices?: proto.cosmos.base.v1beta1.ICoin[] | null;
+  @Input()
+  validator?: InlineResponse20066Validators | null;
+
+  @Output()
+  appSubmit: EventEmitter<DelegateOnSubmitEvent>;
+
+  selectedGasPrice?: proto.cosmos.base.v1beta1.ICoin;
+  availableDenoms?: string[];
+  selectedAmount?: proto.cosmos.base.v1beta1.ICoin;
+
+  constructor() {
+    this.appSubmit = new EventEmitter();
+    this.availableDenoms = this.coins?.map((coin) => coin.denom!);
+    if (this.coins) {
+      this.selectedAmount = { denom: this.coins[0].denom, amount: '0' };
+    }
+  }
+
+  ngOnChanges(): void {
+    if (this.minimumGasPrices && this.minimumGasPrices.length > 0) {
+      this.selectedGasPrice = this.minimumGasPrices[0];
+    }
+  }
 
   ngOnInit(): void {}
 
@@ -29,7 +56,27 @@ export class DelegateValidatorDialogComponent implements OnInit {
     return `#${hash.substr(0, 6)}`;
   }
 
-  onClickButton(): void {
-    this.matDialogRef.close();
+  onSubmit(minimumGasPrice: string) {
+    if (!this.selectedAmount) {
+      return;
+    }
+    if (this.selectedGasPrice === undefined) {
+      return;
+    }
+    this.selectedGasPrice.amount = minimumGasPrice.toString();
+
+    this.appSubmit.emit({ amount: this.selectedAmount, minimumGasPrice: this.selectedGasPrice });
+  }
+
+  onMinimumGasDenomChanged(denom: string): void {
+    this.selectedGasPrice = this.minimumGasPrices?.find(
+      (minimumGasPrice) => minimumGasPrice.denom === denom,
+    );
+  }
+
+  onMinimumGasAmountSliderChanged(amount: string): void {
+    if (this.selectedGasPrice) {
+      this.selectedGasPrice.amount = amount;
+    }
   }
 }
--- projects/portal/src/app/views/dialogs/delegate/delegate-validator-dialog/delegate-validator-dialog.module.ts
+++ projects/portal/src/app/views/dialogs/delegate/delegate-validator-dialog/delegate-validator-dialog.module.ts
@@ -2,10 +2,11 @@ import { MaterialModule } from '../../../material.module';
 import { DelegateValidatorDialogComponent } from './delegate-validator-dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DelegateValidatorDialogComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, FormsModule, MaterialModule],
   exports: [DelegateValidatorDialogComponent],
 })
 export class DelegateValidatorDialogModule {}

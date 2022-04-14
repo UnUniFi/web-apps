b260aafafb7ea96d0db4fac968e9c0da90396d74
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-04-14 21:41:19 +0900

    feat: fix config service bech32prefix

--- projects/explorer/src/app/models/cosmos-sdk.service.ts
+++ projects/explorer/src/app/models/cosmos-sdk.service.ts
@@ -1,7 +1,7 @@
-import { ConfigService } from './config.service';
+import { Config, ConfigService } from './config.service';
 import { Injectable } from '@angular/core';
 import { cosmosclient } from '@cosmos-client/core';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
 
 @Injectable({
@@ -14,41 +14,79 @@ export class CosmosSDKService {
   sdk$: Observable<{ rest: cosmosclient.CosmosSDK; websocket: cosmosclient.CosmosSDK }>;
 
   constructor(private readonly configS: ConfigService) {
-    this.configS.config$.pipe(
-      map((config) => {
-        if (
-          config &&
-          config.bech32Prefix?.accAddr &&
-          config.bech32Prefix?.accPub &&
-          config.bech32Prefix?.valAddr &&
-          config.bech32Prefix?.valAddr &&
-          config.bech32Prefix?.consAddr &&
-          config.bech32Prefix?.consAddr
-        ) {
-          cosmosclient.config.setBech32Prefix({
-            accAddr: config.bech32Prefix?.accAddr,
-            accPub: config.bech32Prefix?.accPub,
-            valAddr: config.bech32Prefix?.valAddr,
-            valPub: config.bech32Prefix?.valAddr,
-            consAddr: config.bech32Prefix?.consAddr,
-            consPub: config.bech32Prefix?.consAddr,
-          });
-        }
-      }),
-    );
-
     this.restURL$ = this.configS.config$.pipe(map((config) => config?.restURL!));
     this.websocketURL$ = this.configS.config$.pipe(map((config) => config?.websocketURL!));
     this.chainID$ = this.configS.config$.pipe(map((config) => config?.chainID!));
-    this.sdk$ = combineLatest([this.restURL$, this.websocketURL$, this.chainID$]).pipe(
-      map(([restURL, websocketURL, chainID]) => ({
-        rest: new cosmosclient.CosmosSDK(restURL, chainID),
-        websocket: new cosmosclient.CosmosSDK(websocketURL, chainID),
-      })),
+    this.sdk$ = combineLatest([
+      this.configS.config$,
+      this.restURL$,
+      this.websocketURL$,
+      this.chainID$,
+    ]).pipe(
+      map(([config, restURL, websocketURL, chainID]) => {
+        if (!config) {
+          return {
+            rest: new cosmosclient.CosmosSDK(restURL, chainID),
+            websocket: new cosmosclient.CosmosSDK(websocketURL, chainID),
+          };
+        }
+        this.setBech32PrefixToCosmosclient(config, restURL, websocketURL, chainID);
+        return {
+          rest: new cosmosclient.CosmosSDK(restURL, chainID),
+          websocket: new cosmosclient.CosmosSDK(websocketURL, chainID),
+        };
+      }),
     );
   }
 
   sdk() {
     return this.sdk$.pipe(first()).toPromise();
+  }
+
+  isValidPairConfigRestURLWebsocketURLChainID(
+    config: Config,
+    restURL: string,
+    websocketURL: string,
+    chainID: string,
+  ): boolean {
+    if (config.restURL !== restURL) {
+      return false;
+    }
+    if (config.websocketURL !== websocketURL) {
+      return false;
+    }
+    if (config.chainID !== chainID) {
+      return false;
+    }
+    return true;
+  }
+
+  setBech32PrefixToCosmosclient(
+    config: Config,
+    restURL: string,
+    websocketURL: string,
+    chainID: string,
+  ) {
+    if (!this.isValidPairConfigRestURLWebsocketURLChainID(config, restURL, websocketURL, chainID)) {
+      return;
+    }
+    if (
+      config &&
+      config.bech32Prefix?.accAddr &&
+      config.bech32Prefix?.accPub &&
+      config.bech32Prefix?.valAddr &&
+      config.bech32Prefix?.valAddr &&
+      config.bech32Prefix?.consAddr &&
+      config.bech32Prefix?.consAddr
+    ) {
+      cosmosclient.config.setBech32Prefix({
+        accAddr: config.bech32Prefix?.accAddr,
+        accPub: config.bech32Prefix?.accPub,
+        valAddr: config.bech32Prefix?.valAddr,
+        valPub: config.bech32Prefix?.valAddr,
+        consAddr: config.bech32Prefix?.consAddr,
+        consPub: config.bech32Prefix?.consAddr,
+      });
+    }
   }
 }

653bce95a050559c91d938358bcca17fdf13137b
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-14 16:17:18 +0900

    feat: delegate-menu-dialog

---
+++ projects/portal/src/app/pages/dialogs/delegate/delegate-menu-dialog/delegate-menu-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delegate-menu-dialog',
+  templateUrl: './delegate-menu-dialog.component.html',
+  styleUrls: ['./delegate-menu-dialog.component.css']
+})
+export class DelegateMenuDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/portal/src/app/views/dialogs/delegate/delegate-menu-dialog/delegate-menu-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-delegate-menu-dialog',
+  templateUrl: './delegate-menu-dialog.component.html',
+  styleUrls: ['./delegate-menu-dialog.component.css'],
+})
+export class DelegateMenuDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

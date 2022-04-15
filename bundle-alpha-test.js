138a9ebf19c1daf042936e1dd2b7d39a16956c35
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-15 14:40:25 +0900

    fix: file path

--- bundle-alpha-test.js
+++ bundle-alpha-test.js
@@ -4,4 +4,4 @@ fse.copySync("dist/explorer", "dist/landing-page/explorer");
 fse.copySync("dist/portal", "dist/landing-page/portal");
 
 fse.copyFileSync("projects/explorer/editions/ununifi/launch/ununifi-alpha-test/firebase-hosting/config.js", "dist/landing-page/explorer/assets/config.js");
-fse.copyFileSync("projects/portal/editions/ununifi/launch/ununifi-alpha-test/firebase-hosting/config.js", "dist/landing-page/portal");
+fse.copyFileSync("projects/portal/editions/ununifi/launch/ununifi-alpha-test/firebase-hosting/config.js", "dist/landing-page/portal/assets/config.js");

2fa5c44b7dd9aa73e42117dd5acf2dfd987d5511
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-15 14:31:15 +0900

    feat: cd with github actions and bundle script

---
+++ bundle-ununifi-8-private-test.js
@@ -0,0 +1,7 @@
+const fse = require("fs-extra");
+
+fse.copySync("dist/explorer", "dist/landing-page/explorer");
+fse.copySync("dist/portal", "dist/landing-page/portal");
+
+fse.copyFileSync("projects/explorer/editions/ununifi/launch/ununifi-8-private-test/firebase-hosting/config.js", "dist/landing-page/explorer/assets/config.js");
+fse.copyFileSync("projects/portal/editions/ununifi/launch/ununifi-8-private-test/firebase-hosting/config.js", "dist/landing-page/portal");

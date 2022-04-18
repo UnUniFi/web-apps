a989a08d0fdb0bb6268f1c32a78cd1688c10c9ef
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-04-18 18:13:15 +0900

    feat: proposal list

--- projects/portal/src/app/views/vote/proposals/proposals.component.ts
+++ projects/portal/src/app/views/vote/proposals/proposals.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { cosmosclient } from '@cosmos-client/core';
 import { InlineResponse20052 } from '@cosmos-client/core/esm/openapi';
+
+export interface ProposalContent {
+  type: string;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'view-proposals',
@@ -13,4 +20,12 @@ export class ProposalsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  unpackContent(value: any) {
+    try {
+      return cosmosclient.codec.unpackCosmosAny(value) as ProposalContent;
+    } catch {
+      return null;
+    }
+  }
 }

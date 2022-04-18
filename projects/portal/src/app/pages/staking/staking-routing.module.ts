2604a83c4dbb7ec37261536a0a886746a677bb48
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-04-18 17:35:09 +0900

    feat: copy from create validator

--- projects/portal/src/app/pages/staking/staking-routing.module.ts
+++ projects/portal/src/app/pages/staking/staking-routing.module.ts
@@ -1,5 +1,6 @@
 import { WalletGuard } from '../../models/wallets/wallet.guard';
 import { CreateValidatorComponent } from './create-validator/create-validator.component';
+import { SimpleComponent } from './create-validator/simple/simple.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,6 +8,11 @@ const routes: Routes = [
   {
     path: 'create-validator',
     component: CreateValidatorComponent,
+    canActivate: [WalletGuard],
+  },
+  {
+    path: 'create-validator/simple',
+    component: SimpleComponent,
     canActivate: [WalletGuard],
   },
 ];
--- projects/portal/src/app/pages/staking/staking.module.ts
+++ projects/portal/src/app/pages/staking/staking.module.ts
@@ -1,12 +1,14 @@
 import { CreateValidatorModule } from '../../views/staking/create-validator/create-validator.module';
+import { SimpleModule } from '../../views/staking/create-validator/simple/simple.module';
 import { StakingModule } from '../../views/staking/staking.module';
 import { CreateValidatorComponent } from './create-validator/create-validator.component';
+import { SimpleComponent } from './create-validator/simple/simple.component';
 import { StakingRoutingModule } from './staking-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [CreateValidatorComponent],
-  imports: [CommonModule, StakingRoutingModule, CreateValidatorModule, StakingModule],
+  declarations: [CreateValidatorComponent, SimpleComponent],
+  imports: [CommonModule, StakingRoutingModule, CreateValidatorModule, StakingModule, SimpleModule],
 })
 export class AppStakingModule {}

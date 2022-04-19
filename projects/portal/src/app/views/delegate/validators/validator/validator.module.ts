fa3c4f6a991847e25b946cb1a9cd1e96ef7574a4
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-04-19 13:45:21 +0900

    fix: portal/validator detail page design

--- projects/portal/src/app/views/delegate/validators/validator/validator.module.ts
+++ projects/portal/src/app/views/delegate/validators/validator/validator.module.ts
@@ -2,11 +2,12 @@ import { MaterialModule } from '../../../material.module';
 import { ValidatorComponent } from './validator.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ValidatorComponent],
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule],
   exports: [ValidatorComponent],
 })
 export class ValidatorModule {}

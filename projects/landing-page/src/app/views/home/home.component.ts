3c0e3edfe7fc17f21ebf58bd0ac3ad252bba59c2
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-04-26 00:24:54 +0900

    feat: langing page

--- projects/landing-page/src/app/views/home/home.component.ts
+++ projects/landing-page/src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'view-home',
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  @ViewChild('toggled') myNameElem?: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {}
+
+  toggler() {
+    //getValue() {
+    console.log('may', this.myNameElem);
+
+    const toggledIsActive = this.myNameElem?.nativeElement.classList.contains('d-none');
+    //const hamSmallIsActive = this.hamburgerSmall.nativeElement.classList.contains('is-active');
+    //const menuShow = this.menu.nativeElement.classList.contains('show');
+
+    //this.myNameElem?.nativeElement?.innerHTML  = "I am changed by ElementRef & ViewChild";
+    //this.myNameElem?.nativeElement?.getElementById('toggled').classList.toggle('d-none');
+    console.log('gine');
+
+    if (toggledIsActive) {
+      this.renderer.removeClass(this.myNameElem?.nativeElement, 'is-active');
+    } else {
+      this.renderer.addClass(this.myNameElem?.nativeElement, 'is-active');
+    }
+  }
 }

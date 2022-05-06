eca70423f21a88cad28aa218e6122e8264824862
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-05-06 19:46:00 +0900

    feat: add pagination to txs card in account page

--- projects/explorer/src/app/views/accounts/account/txs/txs.component.ts
+++ projects/explorer/src/app/views/accounts/account/txs/txs.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { PaginationInfo } from '../../../../pages/accounts/account/txs/txs.component';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { CosmosTxV1beta1GetTxsEventResponse } from '@cosmos-client/core/esm/openapi';
 
 @Component({
@@ -9,7 +11,21 @@ import { CosmosTxV1beta1GetTxsEventResponse } from '@cosmos-client/core/esm/open
 export class TxsComponent implements OnInit {
   @Input() txsWithPagination?: CosmosTxV1beta1GetTxsEventResponse | null;
 
-  constructor() {}
+  @Input() pageSizeOptions?: number[] | null;
+
+  @Input() pageInfo?: PaginationInfo | null;
+
+  @Input() pageLength?: number | null;
+
+  @Output() paginationChange: EventEmitter<PageEvent>;
+
+  constructor() {
+    this.paginationChange = new EventEmitter();
+  }
 
   ngOnInit(): void {}
+
+  onPaginationChange(pageEvent: PageEvent): void {
+    this.paginationChange.emit(pageEvent);
+  }
 }

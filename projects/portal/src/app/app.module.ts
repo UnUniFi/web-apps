9f18c8c250761774a5d51a53f40cee578a6201cd
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-05-07 22:27:09 +0900

    feat: import wallet with private key

--- projects/portal/src/app/app.module.ts
+++ projects/portal/src/app/app.module.ts
@@ -13,8 +13,10 @@ import { InactiveValidatorModule } from './views/dialogs/delegate/invalid-valida
 import { ConnectWalletCompletedDialogModule } from './views/dialogs/wallets/connect-wallet-completed-dialog/connect-wallet-completed-dialog.module';
 import { ConnectWalletStartDialogModule } from './views/dialogs/wallets/connect-wallet-start-dialog/connect-wallet-start-dialog.module';
 import { UnunifiBackupMnemonicAndPrivateKeyWizardDialogModule } from './views/dialogs/wallets/ununifi/ununifi-backup-mnemonic-and-private-key-wizard-dialog/ununifi-backup-mnemonic-and-private-key-wizard-dialog.module';
+import { UnunifiBackupPrivateKeyWizardDialogModule } from './views/dialogs/wallets/ununifi/ununifi-backup-private-key-wizard-dialog/ununifi-backup-private-key-wizard-dialog.module';
 import { UnunifiCreateWalletFormDialogModule } from './views/dialogs/wallets/ununifi/ununifi-create-wallet-form-dialog/ununifi-create-wallet-form-dialog.module';
 import { UnunifiImportWalletWithMnemonicFormDialogModule } from './views/dialogs/wallets/ununifi/ununifi-import-wallet-with-mnemonic-form-dialog/ununifi-import-wallet-with-mnemonic-form-dialog.module';
+import { UnunifiImportWalletWithPrivateKeyFormDialogModule } from './views/dialogs/wallets/ununifi/ununifi-import-wallet-with-private-key-form-dialog/ununifi-import-wallet-with-private-key-form-dialog.module';
 import { UnunifiKeyFormDialogModule } from './views/dialogs/wallets/ununifi/ununifi-key-form-dialog/ununifi-key-form-dialog.module';
 import { UnunifiSelectCreateImportDialogModule } from './views/dialogs/wallets/ununifi/ununifi-select-create-import-dialog/ununifi-select-create-import-dialog.module';
 import { UnunifiSelectWalletDialogModule } from './views/dialogs/wallets/ununifi/ununifi-select-wallet-dialog/ununifi-select-wallet-dialog.module';
@@ -58,8 +60,10 @@ import { LoadingDialogModule } from 'ng-loading-dialog';
     ConnectWalletCompletedDialogModule,
     ConnectWalletStartDialogModule,
     UnunifiBackupMnemonicAndPrivateKeyWizardDialogModule,
+    UnunifiBackupPrivateKeyWizardDialogModule,
     UnunifiCreateWalletFormDialogModule,
     UnunifiImportWalletWithMnemonicFormDialogModule,
+    UnunifiImportWalletWithPrivateKeyFormDialogModule,
     UnunifiSelectCreateImportDialogModule,
     UnunifiSelectWalletDialogModule,
     UnunifiKeyFormDialogModule,

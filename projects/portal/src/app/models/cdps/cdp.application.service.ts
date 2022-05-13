f4cc83801cc70e330a5f68ad63f8778844c9d810
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-05-13 14:33:23 +0900

    feat: service support new sign and keplr function

--- projects/portal/src/app/models/cdps/cdp.application.service.ts
+++ projects/portal/src/app/models/cdps/cdp.application.service.ts
@@ -2,6 +2,8 @@ import { TxFeeConfirmDialogComponent } from '../../views/cosmos/tx-fee-confirm-d
 import { SimulatedTxResultResponse } from '../cosmos/tx-common.model';
 import { Key } from '../keys/key.model';
 import { KeyService } from '../keys/key.service';
+import { WalletType } from '../wallets/wallet.model';
+import { WalletService } from '../wallets/wallet.service';
 import { CdpService } from './cdp.service';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
@@ -10,6 +12,7 @@ import { Router } from '@angular/router';
 import { cosmosclient, proto } from '@cosmos-client/core';
 import { InlineResponse20075 } from '@cosmos-client/core/esm/openapi';
 import { LoadingDialogService } from 'ng-loading-dialog';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -22,11 +25,10 @@ export class CdpApplicationService {
     private readonly loadingDialog: LoadingDialogService,
     private readonly cdp: CdpService,
     private readonly key: KeyService,
+    private readonly walletService: WalletService,
   ) {}
 
   async createCDP(
-    key: Key,
-    privateKey: Uint8Array,
     collateralType: string,
     collateral: proto.cosmos.base.v1beta1.ICoin,
     principal: proto.cosmos.base.v1beta1.ICoin,
@@ -34,39 +36,31 @@ export class CdpApplicationService {
     balances: proto.cosmos.base.v1beta1.ICoin[],
     gasRatio: number,
   ) {
-    // validation
-    if (!(await this.key.validatePrivKey(key, privateKey))) {
-      this.snackBar.open(`Invalid private key.`, 'Close');
-      return;
+    // get public key
+    const currentCosmosWallet = await this.walletService.currentCosmosWallet$
+      .pipe(take(1))
+      .toPromise();
+    if (!currentCosmosWallet) {
+      throw Error('Current connected wallet is invalid!');
+    }
+    const cosmosPublicKey = currentCosmosWallet.public_key;
+    if (!cosmosPublicKey) {
+      throw Error('Invalid public key!');
     }
 
     // simulate
     let simulatedResultData: SimulatedTxResultResponse;
     let gas: proto.cosmos.base.v1beta1.ICoin;
     let fee: proto.cosmos.base.v1beta1.ICoin;
-    const dialogRefSimulating = this.loadingDialog.open('Simulating...');
 
-    // confirm whether account has enough gas denom for simulation
-    const feeDenom = minimumGasPrice.denom;
-    const simulationFeeAmount = 1;
-    const tempBalance = balances.find((balance) => balance.denom === minimumGasPrice.denom)?.amount;
-    const balance = tempBalance ? parseInt(tempBalance) : 0;
-    if (simulationFeeAmount > balance) {
-      this.snackBar.open(
-        `Insufficient fee margin for simulation!\n Simulation fee: ${simulationFeeAmount}${feeDenom} > Balance: ${balance}${feeDenom}`,
-        'Close',
-      );
-      dialogRefSimulating.close();
-      return;
-    }
+    const dialogRefSimulating = this.loadingDialog.open('Simulating...');
 
     try {
       simulatedResultData = await this.cdp.simulateToCreateCDP(
-        key,
-        privateKey,
         collateralType,
         collateral,
         principal,
+        cosmosPublicKey,
         minimumGasPrice,
         gasRatio,
       );
@@ -81,47 +75,39 @@ export class CdpApplicationService {
       dialogRefSimulating.close();
     }
 
-    // check whether the fee exceeded
-    const simulatedFee = fee.amount ? parseInt(fee.amount) : 0;
-    if (simulatedFee > balance) {
-      this.snackBar.open(
-        `Insufficient fee margin for Create!\n Simulated fee: ${simulatedFee}${feeDenom} > Balance: ${balance}${feeDenom}`,
-        'Close',
-      );
-      return;
+    // confirm fee only ununifi wallet type case
+    if (currentCosmosWallet.type === WalletType.ununifi) {
+      const txFeeConfirmedResult = await this.dialog
+        .open(TxFeeConfirmDialogComponent, {
+          data: {
+            fee,
+            isConfirmed: false,
+          },
+        })
+        .afterClosed()
+        .toPromise();
+      if (txFeeConfirmedResult === undefined || txFeeConfirmedResult.isConfirmed === false) {
+        this.snackBar.open('Tx was canceled', undefined, { duration: 6000 });
+        return;
+      }
     }
 
-    // ask the user to confirm the fee with a dialog
-    const txFeeConfirmedResult = await this.dialog
-      .open(TxFeeConfirmDialogComponent, {
-        data: {
-          fee,
-          isConfirmed: false,
-        },
-      })
-      .afterClosed()
-      .toPromise();
-
-    if (txFeeConfirmedResult === undefined || txFeeConfirmedResult.isConfirmed === false) {
-      this.snackBar.open('Tx was canceled', undefined, { duration: 6000 });
-      return;
-    }
-
+    // send tx
     const dialogRef = this.loadingDialog.open('Sending...');
 
-    let txhash: string | undefined;
+    let txResult: InlineResponse20075 | undefined;
+    let txHash: string | undefined;
     try {
-      const res: InlineResponse20075 = await this.cdp.createCDP(
-        key,
-        privateKey,
+      txResult = await this.cdp.createCDP(
         collateralType,
         collateral,
         principal,
+        currentCosmosWallet,
         gas,
         fee,
       );
-      txhash = res.tx_response?.txhash;
-      if (txhash === undefined) {
+      txHash = txResult.tx_response?.txhash;
+      if (txHash === undefined) {
         throw Error('invalid txhash!');
       }
     } catch (error) {
@@ -137,7 +123,7 @@ export class CdpApplicationService {
       duration: 6000,
     });
 
-    const redirectUrl = `${location.protocol}//${location.hostname}/txs/${txhash}`;
+    const redirectUrl = `${location.protocol}//${location.hostname}/txs/${txHash}`;
     window.location.href = redirectUrl;
   }
 
--- projects/portal/src/app/models/cdps/cdp.infrastructure.service.ts
+++ projects/portal/src/app/models/cdps/cdp.infrastructure.service.ts
@@ -3,6 +3,7 @@ import { SimulatedTxResultResponse } from '../cosmos/tx-common.model';
 import { TxCommonService } from '../cosmos/tx-common.service';
 import { Key } from '../keys/key.model';
 import { KeyService } from '../keys/key.service';
+import { CosmosWallet } from '../wallets/wallet.model';
 import { ICdpInfrastructure } from './cdp.service';
 import { Injectable } from '@angular/core';
 import { cosmosclient, proto, rest } from '@cosmos-client/core';
@@ -21,32 +22,45 @@ export class CdpInfrastructureService implements ICdpInfrastructure {
   ) {}
 
   async createCDP(
-    key: Key,
-    privateKey: Uint8Array,
     collateralType: string,
     collateral: proto.cosmos.base.v1beta1.ICoin,
     principal: proto.cosmos.base.v1beta1.ICoin,
+    currentCosmosWallet: CosmosWallet,
     gas: proto.cosmos.base.v1beta1.ICoin,
     fee: proto.cosmos.base.v1beta1.ICoin,
+    privateKey?: string,
   ): Promise<InlineResponse20075> {
-    const txBuilder = await this.buildCreateCDPTx(
-      key,
-      privateKey,
+    const cosmosPublicKey = currentCosmosWallet.public_key;
+    const txBuilder = await this.buildCreateCDPTxBuilder(
       collateralType,
       collateral,
       principal,
+      cosmosPublicKey,
       gas,
       fee,
     );
-    return await this.txCommonService.announceTx(txBuilder);
+    const signerBaseAccount = await this.txCommonService.getBaseAccount(cosmosPublicKey);
+    if (!signerBaseAccount) {
+      throw Error('Unsupported Account!');
+    }
+    const signedTxBuilder = await this.txCommonService.signTx(
+      txBuilder,
+      signerBaseAccount,
+      currentCosmosWallet,
+      privateKey,
+    );
+    if (!signedTxBuilder) {
+      throw Error('Failed to sign!');
+    }
+    const txResult = await this.txCommonService.announceTx(signedTxBuilder);
+    return txResult;
   }
 
   async simulateToCreateCDP(
-    key: Key,
-    privateKey: Uint8Array,
     collateralType: string,
     collateral: proto.cosmos.base.v1beta1.ICoin,
     principal: proto.cosmos.base.v1beta1.ICoin,
+    cosmosPublicKey: cosmosclient.PubKey,
     minimumGasPrice: proto.cosmos.base.v1beta1.ICoin,
     gasRatio: number,
   ): Promise<SimulatedTxResultResponse> {
@@ -58,43 +72,31 @@ export class CdpInfrastructureService implements ICdpInfrastructure {
       denom: minimumGasPrice.denom,
       amount: '1',
     };
-    const simulatedTxBuilder = await this.buildCreateCDPTx(
-      key,
-      privateKey,
+    const simulatedTxBuilder = await this.buildCreateCDPTxBuilder(
       collateralType,
       collateral,
       principal,
+      cosmosPublicKey,
       dummyGas,
       dummyFee,
     );
     return await this.txCommonService.simulateTx(simulatedTxBuilder, minimumGasPrice, gasRatio);
   }
 
-  async buildCreateCDPTx(
-    key: Key,
-    privateKey: Uint8Array,
+  async buildCreateCDPTxBuilder(
     collateralType: string,
     collateral: proto.cosmos.base.v1beta1.ICoin,
     principal: proto.cosmos.base.v1beta1.ICoin,
+    cosmosPublicKey: cosmosclient.PubKey,
     gas: proto.cosmos.base.v1beta1.ICoin,
     fee: proto.cosmos.base.v1beta1.ICoin,
   ): Promise<cosmosclient.TxBuilder> {
-    const sdk = await this.cosmosSDK.sdk();
-    const privKey = this.keyService.getPrivKey(key.type, privateKey);
-    const pubKey = privKey.pubKey();
-    const sender = cosmosclient.AccAddress.fromPublicKey(privKey.pubKey());
-
     // get account info
-    const account = await rest.auth
-      .account(sdk.rest, sender)
-      .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account))
-      .catch((_) => undefined);
-
-    const baseAccount = convertUnknownAccountToBaseAccount(account);
-
+    const baseAccount = await this.txCommonService.getBaseAccount(cosmosPublicKey);
     if (!baseAccount) {
       throw Error('Unused Account or Unsupported Account Type!');
     }
+    const sender = cosmosclient.AccAddress.fromPublicKey(cosmosPublicKey);
 
     // build tx
     const msgCreateCdp = new ununifi.cdp.MsgCreateCdp({
@@ -104,32 +106,13 @@ export class CdpInfrastructureService implements ICdpInfrastructure {
       collateral_type: collateralType,
     });
 
-    const txBody = new proto.cosmos.tx.v1beta1.TxBody({
-      messages: [cosmosclient.codec.packAny(msgCreateCdp)],
-    });
-    const authInfo = new proto.cosmos.tx.v1beta1.AuthInfo({
-      signer_infos: [
-        {
-          public_key: cosmosclient.codec.packAny(pubKey),
-          mode_info: {
-            single: {
-              mode: proto.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT,
-            },
-          },
-          sequence: baseAccount.sequence,
-        },
-      ],
-      fee: {
-        amount: [fee],
-        gas_limit: cosmosclient.Long.fromString(gas.amount ? gas.amount : '300000'),
-      },
-    });
-
-    // sign
-    const txBuilder = new cosmosclient.TxBuilder(sdk.rest, txBody, authInfo);
-    const signDocBytes = txBuilder.signDocBytes(baseAccount.account_number);
-    txBuilder.addSignature(privKey.sign(signDocBytes));
-
+    const txBuilder = await this.txCommonService.buildTxBuilder(
+      [msgCreateCdp],
+      cosmosPublicKey,
+      baseAccount,
+      gas,
+      fee,
+    );
     return txBuilder;
   }
 

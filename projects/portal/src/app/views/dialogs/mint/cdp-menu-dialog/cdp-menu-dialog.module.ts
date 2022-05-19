3eda3292150fb8616ed948210fedb38318d610ed
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2022-05-19 16:59:09 +0900

    feat: cdp-menu-dialog

--- projects/portal/src/app/views/dialogs/mint/cdp-menu-dialog/cdp-menu-dialog.module.ts
+++ projects/portal/src/app/views/dialogs/mint/cdp-menu-dialog/cdp-menu-dialog.module.ts
@@ -1,11 +1,12 @@
 import { CdpMenuDialogComponent } from './cdp-menu-dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'projects/explorer/src/app/views/material.module';
 
 @NgModule({
   declarations: [CdpMenuDialogComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   exports: [CdpMenuDialogComponent],
 })
 export class CdpMenuDialogModule {}

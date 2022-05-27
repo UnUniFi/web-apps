15f4aae8c396e18f571c051280dd27938c498404
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-05-27 20:04:49 +0900

    chore: install web3 and metamask related npm packages and resolve build error
    https://stackoverflow.com/questions/71092985/angular-13-clean-install-wouldnt-work-with-web3-js

--- projects/portal/src/polyfills.ts
+++ projects/portal/src/polyfills.ts
@@ -57,11 +57,17 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
+import 'zone.js';
 
+// Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
 (window as any).global = window;
 global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = {
+  env: { DEBUG: undefined },
+  version: '',
+  nextTick: require('next-tick'),
+} as any;

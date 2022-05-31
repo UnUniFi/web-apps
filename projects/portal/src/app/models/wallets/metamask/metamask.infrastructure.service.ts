a437fea8d37fb6618181af4e762358e8ecfbf66f
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-05-31 20:36:41 +0900

    feat: wip sign to cosmos sdk tx data with metamask and broadcast
    signature verification failed

--- projects/portal/src/app/models/wallets/metamask/metamask.infrastructure.service.ts
+++ projects/portal/src/app/models/wallets/metamask/metamask.infrastructure.service.ts
@@ -1,113 +1,23 @@
 import { createCosmosPublicKeyFromString } from '../../../utils/key';
+import { ConfigService } from '../../config.service';
+import { MetaMaskTx } from '../../cosmos/tx-common.model';
 import { KeyType } from '../../keys/key.model';
 import { StoredWallet, WalletType } from '../wallet.model';
 import { IMetaMaskInfrastructureService } from './metamask.service';
 import { Injectable } from '@angular/core';
-import { cosmosclient } from '@cosmos-client/core';
+import { cosmosclient, proto } from '@cosmos-client/core';
 import detectEthereumProvider from '@metamask/detect-provider';
 import { extractPublicKey } from 'eth-sig-util';
+import { take } from 'rxjs/operators';
 import Web3 from 'web3';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureService {
-  constructor() {}
+  constructor(private readonly configService: ConfigService) {}
 
-  // Todo: message to be signed json and it's schema type should be set to function arguments.
-  async signTypedDataV4ToJsonWithMetaMask(): Promise<string> {
-    const provider = await detectEthereumProvider({ mustBeMetaMask: true });
-    if (!provider || !window.ethereum?.isMetaMask) {
-      throw Error('MetaMask is not installed!');
-    }
-
-    const accounts = await (provider as any).request({ method: 'eth_requestAccounts' });
-    if (!accounts?.length) {
-      throw Error('Failed to get MetaMask account!');
-    }
-    const metamaskAddress = accounts[0];
-
-    const web3 = new Web3(Web3.givenProvider);
-    const chainId = await web3.eth.getChainId();
-    console.log('chainId', chainId);
-    console.log('accounts', accounts);
-    console.log('ethAddress', metamaskAddress);
-
-    // Todo: This should be set function arguments instead of hard-coding.
-    const cosmosLegacyAminoTransactionJson = {
-      account_number: '12',
-      chain_id: 'test',
-      fee: { amount: [], gas: '200000' },
-      memo: '',
-      msgs: [
-        {
-          type: 'cosmos-sdk/MsgSend',
-          value: {
-            amount: [{ amount: '2000000', denom: 'uguu' }],
-            from_address: 'ununifi1tqlgm42t0sl4ag3cvtsws5hsu6g5gaw4zqxeev',
-            to_address: 'ununifi1v0lz8djhfakyf6k5cjez49nt0d657evwp5xru0',
-          },
-        },
-      ],
-      sequence: '0',
-    };
-
-    // Todo: I would like to automatically generate the types of the message from the message type and schema.
-    // Todo: If not, this should be set function arguments.
-    const messageJson = {
-      domain: {
-        chainId,
-        name: 'UnUniFi',
-        version: '1',
-      },
-      message: cosmosLegacyAminoTransactionJson,
-      primaryType: 'Message',
-      types: {
-        EIP712Domain: [
-          { name: 'name', type: 'string' },
-          { name: 'version', type: 'string' },
-          { name: 'chainId', type: 'uint256' },
-        ],
-        Message: [
-          { name: 'account_number', type: 'string' },
-          { name: 'chain_id', type: 'string' },
-          { name: 'fee', type: 'Fee' },
-          { name: 'memo', type: 'string' },
-          { name: 'msgs', type: 'Msg[]' },
-          { name: 'sequence', type: 'string' },
-        ],
-        Fee: [
-          { name: 'amount', type: 'Amount[]' },
-          { name: 'gas', type: 'string' },
-        ],
-        Amount: [
-          { name: 'amount', type: 'string' },
-          { name: 'denom', type: 'string' },
-        ],
-        Msg: [
-          { name: 'type', type: 'string' },
-          { name: 'value', type: 'Value' },
-        ],
-        Value: [
-          { name: 'amount', type: 'Amount[]' },
-          { name: 'from_address', type: 'string' },
-          { name: 'to_address', type: 'string' },
-        ],
-      },
-    };
-
-    const messageJsonString = JSON.stringify(messageJson);
-    console.log(messageJsonString);
-
-    const signature = await (provider as any).request({
-      method: 'eth_signTypedData_v4',
-      params: [metamaskAddress, messageJsonString],
-    });
-    console.log('signature', signature);
-
-    return signature;
-  }
-
+  // Note: This method is used to get public key from signature data.
   async signTypedDataV4ToJsonStringWithMetaMask(json: any): Promise<{
     message: string;
     signature: string;
@@ -116,6 +26,7 @@ export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureSer
     return this.signTypedDataV4ToTextWithMetaMask(text);
   }
 
+  // Note: This method is used to get public key from signature data.
   async signTypedDataV4ToTextWithMetaMask(text: string): Promise<{
     message: string;
     signature: string;
@@ -172,7 +83,95 @@ export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureSer
     };
   }
 
-  // Todo: Check the public key is correct or not.
+  // Note: This method is used to sign to Cosmos SDK tx data.
+  async signTypedDataV4ToCosmosSdkTxWithMetaMask(
+    body: string,
+    auth_info: string,
+    account_number: string,
+  ): Promise<{
+    message: string;
+    signature: string;
+  }> {
+    const config = await this.configService.config$.pipe(take(1)).toPromise();
+    const chainId = config?.chainID;
+    if (!chainId) {
+      throw Error('Failed to get UnUniFi ChainID!');
+    }
+
+    const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+    if (!provider || !window.ethereum?.isMetaMask) {
+      throw Error('MetaMask is not installed!');
+    }
+
+    const accounts = await (provider as any).request({ method: 'eth_requestAccounts' });
+    if (!accounts?.length) {
+      throw Error('Failed to get MetaMask account!');
+    }
+    const metamaskAddress = accounts[0];
+
+    const web3 = new Web3(Web3.givenProvider);
+    const metaMaskAccountChainId = await web3.eth.getChainId();
+    console.log('chainId', metaMaskAccountChainId);
+    console.log('accounts', accounts);
+    console.log('ethAddress', metamaskAddress);
+
+    const messageJson: MetaMaskTx = {
+      domain: {
+        chainId: metaMaskAccountChainId,
+        name: 'UnUniFi',
+        version: '1',
+      },
+      message: {
+        auth_info: auth_info,
+        body: body,
+        chain_id: chainId,
+        account_number: account_number,
+      },
+      primaryType: 'Message',
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+        ],
+        Message: [
+          {
+            name: 'body',
+            type: 'string',
+          },
+          {
+            name: 'auth_info',
+            type: 'string',
+          },
+          {
+            name: 'chain_id',
+            type: 'string',
+          },
+          {
+            name: 'account_number',
+            type: 'string',
+          },
+        ],
+      },
+    };
+    console.log(messageJson);
+
+    const messageJsonString = JSON.stringify(messageJson);
+    console.log(messageJsonString);
+
+    const signature = await (provider as any).request({
+      method: 'eth_signTypedData_v4',
+      params: [metamaskAddress, messageJsonString],
+    });
+    console.log('signature', signature);
+
+    return {
+      message: messageJsonString,
+      signature: signature,
+    };
+  }
+
+  // Todo: Public key derived with this method is wrong... This should be fixed...
   async getPublicKeyFromMetaMask(): Promise<string> {
     const signedResult: {
       message: string;
@@ -183,7 +182,7 @@ export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureSer
       sig: signedResult.signature,
     });
     console.log('publicKey', publicKey);
-    // Todo: Check the following compressing way is correct or not.
+    // Todo: Check the following compressing way is correct or not. <- I guess original value before compressing is wrong.
     const x = publicKey.slice(2).slice(0, 64);
     const y = publicKey.slice(2).slice(64);
     const bigIntY = BigInt(`0x${y}`);
@@ -193,7 +192,40 @@ export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureSer
     console.log('y', y);
     console.log('bigIntY', bigIntY);
     console.log('compressedPublicKey', compressedPublicKey);
+
+    const encryptionPublicKey = await this.getEncryptionPublicKeyFromMetaMask();
+    console.log('EncryptionPublicKey', encryptionPublicKey);
+    const hexStringEncryptionPublicKey = Buffer.from(encryptionPublicKey, 'base64').toString('hex');
+    console.log('hexStringEncryptionPublicKey', hexStringEncryptionPublicKey);
+
     return compressedPublicKey;
+  }
+
+  // Note: This method is currently not used. You can delete this.
+  // Note: Public key derived with this method does not match to expected public key...
+  async getEncryptionPublicKeyFromMetaMask(): Promise<string> {
+    const provider = await detectEthereumProvider({ mustBeMetaMask: true });
+    if (!provider || !window.ethereum?.isMetaMask) {
+      throw Error('MetaMask is not installed!');
+    }
+
+    const accounts = await (provider as any).request({ method: 'eth_requestAccounts' });
+    if (!accounts?.length) {
+      throw Error('Failed to get MetaMask account!');
+    }
+    const metamaskAddress = accounts[0];
+
+    const base64EncryptionPublicKey = await (provider as any).request({
+      method: 'eth_getEncryptionPublicKey',
+      params: [metamaskAddress],
+    });
+    console.log('base64EncryptionPublicKey', base64EncryptionPublicKey);
+
+    const hexStringEncryptionPublicKey = Buffer.from(base64EncryptionPublicKey, 'base64').toString(
+      'hex',
+    );
+    console.log('hexStringEncryptionPublicKey', hexStringEncryptionPublicKey);
+    return hexStringEncryptionPublicKey;
   }
 
   async getEthAddress(): Promise<string> {
@@ -232,8 +264,41 @@ export class MetaMaskInfrastructureService implements IMetaMaskInfrastructureSer
     return await this.getCosmosStoredWalletFromMetaMask();
   }
 
-  // Todo: Currently, not implemented yet.
-  async signWithMetaMask(unsignedData: unknown): Promise<Uint8Array | null | undefined> {
-    return undefined;
+  // Todo: This method seems to work well, but, as a result, tx fails. We need to fix signature verification error.
+  async signWithMetaMask(
+    txBuilder: cosmosclient.TxBuilder,
+    signerBaseAccount: proto.cosmos.auth.v1beta1.BaseAccount,
+  ): Promise<cosmosclient.TxBuilder> {
+    txBuilder.signDoc(signerBaseAccount.account_number);
+    const txJsonString = txBuilder.cosmosJSONStringify();
+    if (!txBuilder) {
+      throw Error('Failed to txBuilder');
+    }
+    const txJson = JSON.parse(txJsonString);
+
+    // fix JSONstringify issue
+    delete txJson.auth_info.signer_infos[0].mode_info.multi;
+    console.log(txJson);
+
+    const authInfo = txJson.auth_info;
+    const authInfoJsonString = JSON.stringify(authInfo);
+    const body = txJson.body;
+    const bodyJsonString = JSON.stringify(body);
+    const accountNumber = signerBaseAccount.account_number.toString();
+
+    const signatureResponse = await this.signTypedDataV4ToCosmosSdkTxWithMetaMask(
+      bodyJsonString,
+      authInfoJsonString,
+      accountNumber,
+    );
+    console.log(signatureResponse.message);
+    console.log(signatureResponse.signature);
+
+    const signatureWithout0x = signatureResponse.signature.slice(2);
+    console.log(signatureWithout0x);
+    const signatureUint8Array = Uint8Array.from(Buffer.from(signatureWithout0x, 'hex'));
+    txBuilder.addSignature(signatureUint8Array);
+
+    return txBuilder;
   }
 }

88050ad78c04102effcb9d8b05002d90da3d1f7a
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-16 20:27:51 +0900

    fix: do not build shared and use them directly

--- bundle.js
+++ bundle.js
@@ -1,6 +1,5 @@
 const fse = require("fs-extra");
 
-fse.copySync("dist/shared", "dist/landing-page/shared");
 fse.copySync("dist/explorer", "dist/landing-page/explorer");
 fse.copySync("dist/portal", "dist/landing-page/portal");
 fse.copySync("dist/marketplace", "dist/landing-page/marketplace");

1e12480b5efb56489d8d6b421b8bce921805d9f9
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-22 13:12:49 +0900

    fix: tx simulate response type name
    from InlineResponse20074 to InlineResponse20049

--- projects/shared/src/lib/models/cosmos/tx/common/tx-common.model.ts
+++ projects/shared/src/lib/models/cosmos/tx/common/tx-common.model.ts
@@ -1,8 +1,8 @@
 import { proto } from '@cosmos-client/core';
-import { InlineResponse20074 } from '@cosmos-client/core/esm/openapi';
+import { InlineResponse20049 } from '@cosmos-client/core/esm/openapi';
 
 export type SimulatedTxResultResponse = {
-  simulatedResultData?: InlineResponse20074;
+  simulatedResultData?: InlineResponse20049;
   minimumGasPrice: proto.cosmos.base.v1beta1.ICoin;
   estimatedGasUsedWithMargin: proto.cosmos.base.v1beta1.ICoin;
   estimatedFeeWithMargin: proto.cosmos.base.v1beta1.ICoin;

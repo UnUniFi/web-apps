7f509c34d69c505cdc18fcbae7639734bebf0005
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-07-25 22:16:47 +0900

    fix: add mapper option to jest.config

--- projects/portal/jest.config.js
+++ projects/portal/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/projects/portal/src/setup-jest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/projects/portal/cypress/'],
+  moduleNameMapper: {
+    '@cosmos-client/core': '<rootDir>/node_modules/@cosmos-client/core/cjs/index.js',
+  },
 };

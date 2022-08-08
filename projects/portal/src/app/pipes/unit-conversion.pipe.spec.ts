0f989c8401595ea3bd61d55ceadc81397461ce91
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-08-08 22:46:15 +0900

    fix: minor fix

--- projects/portal/src/app/pipes/unit-conversion.pipe.spec.ts
+++ projects/portal/src/app/pipes/unit-conversion.pipe.spec.ts
@@ -3,19 +3,23 @@ import { UnitConversionPipe } from './unit-conversion.pipe';
 describe('UnitConversionPipe', () => {
   const pipe = new UnitConversionPipe();
 
-  it('It returns K units when input a string number with 4 or more digits', () => {
+  it('It returns K unit when input a string number with 4 or more digits', () => {
     expect(pipe.transform('1234')).toBe('1.23K');
   });
 
-  it('It returns M units when input a number with 7 or more digits', () => {
-    expect(pipe.transform(1234567)).toBe('1.23M');
+  it('It returns M unit when input a number with 7 or more digits', () => {
+    expect(pipe.transform('1234567')).toBe('1.23M');
   });
 
-  it('It returns G units when input a string number with 10 or more digits', () => {
+  it('It returns G unit when input a string number with 10 or more digits', () => {
     expect(pipe.transform('1234567890')).toBe('1.23G');
   });
 
-  it('It returns T units when input a number with 13 or more digits', () => {
+  it('It returns T unit when input a number with 13 or more digits', () => {
+    expect(pipe.transform('1200000000000')).toBe('1.2T');
+  });
+
+  it('It returns string number with a unit when input number', () => {
     expect(pipe.transform(1200000000000)).toBe('1.2T');
   });
 

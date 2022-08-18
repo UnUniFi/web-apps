c387df79edb75c7171c2b5180f6999a2feec9243
taro04 <symbol@DESKTOP-QL7F8DQ.localdomain>
2022-08-18 21:59:56 +0900

    fix: typo

--- projects/marketplace/editions/ununifi/launch/ununifi-alpha-test/firebase-hosting/config.js
+++ projects/marketplace/editions/ununifi/launch/ununifi-alpha-test/firebase-hosting/config.js
@@ -9,7 +9,7 @@ const faucetEuuPort = location.protocol === 'https:' ? 8009 : 8008;
 const domainCauchyEA = 'ununifi-alpha-test-v2.cauchye.net';
 
 const chainID = 'ununifi-alpha-test-v2';
-const chainName = 'UnUnuFi (alpha-test)';
+const chainName = 'UnUniFi (alpha-test)';
 
 const bech32Prefix = {
   accAddr: 'ununifi',

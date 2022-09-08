3b94084d4892b42ead1d02720ec5cf9ee8b922f9
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2022-09-08 11:56:09 +0900

    feat: add loading-dialog

---
+++ projects/shared/src/lib/components/loading-dialog/loading-dialog.component.spec.ts
@@ -0,0 +1,5 @@
+describe('Component', () => {
+  it('should create', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ projects/shared/src/lib/components/loading-dialog/loading-dialog.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
+import {
+  LoadingDialogComponent,
+  LoadingDialogComponentData,
+} from './loading-dialog.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingDialogService {
+  constructor(private dialog: MatDialog) {}
+
+  open(message: string) {
+    const message$ = new BehaviorSubject<string>(message);
+
+    const dialogRef = this.dialog.open<
+      LoadingDialogComponent,
+      LoadingDialogComponentData,
+      undefined
+    >(LoadingDialogComponent, { data: { message$ }, disableClose: true });
+
+    return {
+      next: (message: string) => message$.next(message),
+      close: () => {
+        dialogRef.close();
+        message$.complete();
+      },
+    };
+  }
+}

2b03c8d61c34675c57b46353d2e0cc0da2225d6f
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-12-07 20:58:10 +0900

    feat: incentive page

--- projects/portal/src/app/pages/incentive/incentive.module.ts
+++ projects/portal/src/app/pages/incentive/incentive.module.ts
@@ -1,10 +1,11 @@
 import { IncentiveModule } from '../../views/incentive/incentive.module';
+import { IncentiveRoutingModule } from './incentive-routing.module';
 import { IncentiveComponent } from './incentive.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [IncentiveComponent],
-  imports: [CommonModule, IncentiveModule],
+  imports: [CommonModule, IncentiveRoutingModule, IncentiveModule],
 })
 export class AppIncentiveModule {}
--- projects/portal/src/app/views/incentive/incentive.component.ts
+++ projects/portal/src/app/views/incentive/incentive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import cosmosclient from '@cosmos-client/core';
 
 @Component({
   selector: 'view-incentive',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./incentive.component.css'],
 })
 export class IncentiveComponent implements OnInit {
-  constructor() {}
+  tokens: { id: string }[] | undefined;
+  rewards: cosmosclient.proto.cosmos.base.v1beta1.ICoin[] | undefined;
+
+  constructor() {
+    this.tokens = [{ id: 'ununifi-incentive-test01' }, { id: 'ununifi-incentive-test02' }];
+    this.rewards = [{ denom: 'uguu', amount: '20000000' }];
+  }
 
   ngOnInit(): void {}
 }

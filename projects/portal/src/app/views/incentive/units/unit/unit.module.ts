381486a9504a56820a6e97bb673d5634f9ef13ab
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-01-11 16:48:20 +0900

    feat: add pipe percentage

--- projects/portal/src/app/views/incentive/units/unit/unit.module.ts
+++ projects/portal/src/app/views/incentive/units/unit/unit.module.ts
@@ -4,10 +4,11 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [UnitComponent],
-  imports: [CommonModule, RouterModule, MaterialModule, ClipboardModule],
+  imports: [CommonModule, RouterModule, MaterialModule, ClipboardModule, PipesModule],
   exports: [UnitComponent],
 })
 export class TokenModule {}

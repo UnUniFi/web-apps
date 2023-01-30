0df5b32ce32c22eeefd61bf384c5da3e23bf293f
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-01-30 19:35:54 +0900

    feat: add google-chart

--- projects/portal/src/app/views/yield-aggregator/vaults/vault/vault.module.ts
+++ projects/portal/src/app/views/yield-aggregator/vaults/vault/vault.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 @NgModule({
   declarations: [VaultComponent],
-  imports: [CommonModule, RouterModule, FormsModule, MaterialModule],
+  imports: [CommonModule, RouterModule, FormsModule, MaterialModule, GoogleChartsModule],
   exports: [VaultComponent],
 })
 export class VaultModule {}

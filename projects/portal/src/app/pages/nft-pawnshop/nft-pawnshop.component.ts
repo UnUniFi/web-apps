33d5fdae3c0c22b991e7e4c6addb56b4ff852287
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2023-02-04 17:22:08 +0900

    fix: ci & changes file revert

--- projects/portal/src/app/pages/nft-pawnshop/nft-pawnshop.component.ts
+++ projects/portal/src/app/pages/nft-pawnshop/nft-pawnshop.component.ts
@@ -1,4 +1,4 @@
-import { PawnshopQueryService } from '../../models/nft-pawnshops/nft-pawnshop.query.service';
+import { NftPawnshopQueryService } from '../../models/nft-pawnshops/nft-pawnshop.query.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import {
@@ -17,7 +17,7 @@ export class NftPawnshopComponent implements OnInit {
   listedClasses$: Observable<ListedClass200Response[]>;
   listedNfts$: Observable<ListedNfts200ResponseListingsInner[]>;
 
-  constructor(private readonly pawnshopQuery: PawnshopQueryService) {
+  constructor(private readonly pawnshopQuery: NftPawnshopQueryService) {
     this.params$ = this.pawnshopQuery.getNftmarketParam();
     this.listedClasses$ = this.pawnshopQuery.listListedClasses();
     this.listedNfts$ = this.pawnshopQuery.listListedNfts();

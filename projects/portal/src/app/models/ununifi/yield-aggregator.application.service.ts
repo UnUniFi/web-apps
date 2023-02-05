a382283159126120f2df5df61632d437c43276c4
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-02-05 18:50:43 +0800

    feat: tx common application

--- projects/portal/src/app/models/ununifi/yield-aggregator.application.service.ts
+++ projects/portal/src/app/models/ununifi/yield-aggregator.application.service.ts
@@ -1,9 +1,12 @@
 import { ConfigService } from '../config.service';
 import { BankQueryService } from '../cosmos/bank.query.service';
+import { TxCommonApplicationService } from '../cosmos/tx-common.application.service';
 import { TxCommonService } from '../cosmos/tx-common.service';
 import { WalletService } from '../wallets/wallet.service';
 import { YieldAggregatorService } from './yield-aggregator.service';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { map, take } from 'rxjs/operators';
 
 @Injectable({
@@ -11,76 +14,103 @@ import { map, take } from 'rxjs/operators';
 })
 export class YieldAggregatorApplicationService {
   constructor(
+    private readonly router: Router,
+    private readonly snackBar: MatSnackBar,
     private readonly walletService: WalletService,
     private readonly bankQueryService: BankQueryService,
     private readonly yieldAggregatorService: YieldAggregatorService,
     private readonly txCommon: TxCommonService,
+    private readonly txCommonApplication: TxCommonApplicationService,
     private readonly config: ConfigService,
   ) {}
 
   async deposit(symbol: string, amount: number) {
-    const minimumGasPrice = await this.config.config$
-      .pipe(
-        take(1),
-        map((config) => config?.minimumGasPrices[0]!),
-      )
-      .toPromise();
-    const gasRatio = 1.1;
-
-    // get public key
-    const currentCosmosWallet = await this.walletService.currentCosmosWallet$
-      .pipe(take(1))
-      .toPromise();
-    if (!currentCosmosWallet) {
-      throw Error('Current connected wallet is invalid!');
+    const prerequisiteData = await this.txCommonApplication.getPrerequisiteData();
+    if (!prerequisiteData) {
+      return;
     }
-    const cosmosPublicKey = currentCosmosWallet.public_key;
-    if (!cosmosPublicKey) {
-      throw Error('Invalid public key!');
-    }
-
-    const address = currentCosmosWallet.address.toString();
-    const account = await this.txCommon.getBaseAccountFromAddress(currentCosmosWallet.address);
-    if (!account) {
-      throw Error('Unsupported account type.');
-    }
+    const { address, publicKey, account, currentCosmosWallet, minimumGasPrice } = prerequisiteData;
 
     const symbolMetadataMap = await this.bankQueryService
       .getSymbolMetadataMap$()
       .pipe(take(1))
       .toPromise();
+
+    const msg = this.yieldAggregatorService.buildMsgDeposit(
+      address,
+      symbol,
+      amount,
+      symbolMetadataMap,
+    );
+
+    const simulationResult = await this.txCommonApplication.simulate(
+      msg,
+      publicKey,
+      account,
+      minimumGasPrice,
+    );
+    if (!simulationResult) {
+      return;
+    }
+    const { gas, fee } = simulationResult;
+
+    if (!(await this.txCommonApplication.confirmFeeIfUnUniFiWallet(currentCosmosWallet, fee))) {
+      return;
+    }
+
+    const txHash = await this.txCommonApplication.broadcast(msg, publicKey, account, gas, fee);
+    if (!txHash) {
+      return;
+    }
+
+    this.snackBar.open('Successfully listed NFT.', undefined, {
+      duration: 6000,
+    });
+    await this.router.navigate(['txs', txHash]);
   }
 
   async withdraw(symbol: string, amount: number) {
-    const minimumGasPrice = await this.config.config$
-      .pipe(
-        take(1),
-        map((config) => config?.minimumGasPrices[0]!),
-      )
-      .toPromise();
-    const gasRatio = 1.1;
-
-    // get public key
-    const currentCosmosWallet = await this.walletService.currentCosmosWallet$
-      .pipe(take(1))
-      .toPromise();
-    if (!currentCosmosWallet) {
-      throw Error('Current connected wallet is invalid!');
+    const prerequisiteData = await this.txCommonApplication.getPrerequisiteData();
+    if (!prerequisiteData) {
+      return;
     }
-    const cosmosPublicKey = currentCosmosWallet.public_key;
-    if (!cosmosPublicKey) {
-      throw Error('Invalid public key!');
-    }
-
-    const address = currentCosmosWallet.address.toString();
-    const account = await this.txCommon.getBaseAccountFromAddress(currentCosmosWallet.address);
-    if (!account) {
-      throw Error('Unsupported account type.');
-    }
+    const { address, publicKey, account, currentCosmosWallet, minimumGasPrice } = prerequisiteData;
 
     const symbolMetadataMap = await this.bankQueryService
       .getSymbolMetadataMap$()
       .pipe(take(1))
       .toPromise();
+
+    const msg = this.yieldAggregatorService.buildMsgWithdraw(
+      address,
+      symbol,
+      amount,
+      symbolMetadataMap,
+    );
+
+    const simulationResult = await this.txCommonApplication.simulate(
+      msg,
+      publicKey,
+      account,
+      minimumGasPrice,
+    );
+    if (!simulationResult) {
+      return;
+    }
+    const { gas, fee } = simulationResult;
+
+    if (!(await this.txCommonApplication.confirmFeeIfUnUniFiWallet(currentCosmosWallet, fee))) {
+      return;
+    }
+
+    const txHash = await this.txCommonApplication.broadcast(msg, publicKey, account, gas, fee);
+    if (!txHash) {
+      return;
+    }
+
+    this.snackBar.open('Successfully listed NFT.', undefined, {
+      duration: 6000,
+    });
+    await this.router.navigate(['txs', txHash]);
   }
 }

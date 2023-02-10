b7a3e29b4b971e997d4f106faf66148356ecc809
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-02-10 20:25:07 +0900

    feat: list page

--- projects/portal/src/app/views/nft-pawnshop/borrowers/borrower-nfts/borrower-nft/list/list.component.ts
+++ projects/portal/src/app/views/nft-pawnshop/borrowers/borrower-nfts/borrower-nft/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ListRequest } from 'projects/portal/src/app/models/nft-pawnshops/nft-pawnshop.model';
 import { Metadata } from 'projects/shared/src/lib/models/ununifi/query/nft/nft.model';
+import ununificlient from 'ununifi-client';
 
 @Component({
   selector: 'view-list',
@@ -17,16 +18,36 @@ export class ListComponent implements OnInit {
   @Input()
   nftImage?: string | null;
 
+  bidSymbol: string;
+  minDepositRate: number;
+
   @Output()
   appSubmit: EventEmitter<ListRequest>;
 
   constructor() {
+    this.bidSymbol = 'GUU';
+    this.minDepositRate = 5;
     this.appSubmit = new EventEmitter();
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    this.appSubmit.emit();
+    console.log({
+      classID: this.classID!,
+      nftID: this.nftID!,
+      listingType: ununificlient.proto.ununifi.nftmarket.ListingType.DIRECT_ASSET_BORROW,
+      bidSymbol: this.bidSymbol,
+      minimumDepositRate: this.minDepositRate / 100,
+      autoRefinancing: true,
+    });
+    this.appSubmit.emit({
+      classID: this.classID!,
+      nftID: this.nftID!,
+      listingType: ununificlient.proto.ununifi.nftmarket.ListingType.DIRECT_ASSET_BORROW,
+      bidSymbol: this.bidSymbol,
+      minimumDepositRate: this.minDepositRate / 100,
+      autoRefinancing: true,
+    });
   }
 }
--- projects/portal/src/app/views/nft-pawnshop/borrowers/borrower-nfts/borrower-nft/list/list.module.ts
+++ projects/portal/src/app/views/nft-pawnshop/borrowers/borrower-nfts/borrower-nft/list/list.module.ts
@@ -1,10 +1,12 @@
 import { ListComponent } from './list.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from 'projects/portal/src/app/views/material.module';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, FormsModule],
   exports: [ListComponent],
 })
 export class ListModule {}

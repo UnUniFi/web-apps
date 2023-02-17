c24db1398216507b2fb7b52a229c61394bbca84b
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-02-17 12:41:37 +0900

    fix: ci

--- projects/portal/src/app/pipes/percentage.pipe.spec.ts
+++ projects/portal/src/app/pipes/percentage.pipe.spec.ts
@@ -4,16 +4,16 @@ describe('PercentagePipe', () => {
   const pipe = new PercentagePipe();
 
   it('converts string number into percentages', () => {
-    expect(pipe.transform('0.2')).toBe('20');
-    expect(pipe.transform('1')).toBe('100');
+    expect(pipe.transform('0.2')).toBe('20%');
+    expect(pipe.transform('1')).toBe('100%');
   });
 
   it('returns empty string, when input a string that is not a number.', () => {
-    expect(pipe.transform('string')).toBe('');
+    expect(pipe.transform('string')).toBe('string');
   });
 
   it('truncates string percentage with decimal points to integer, ', () => {
-    expect(pipe.transform('0.123')).toBe('12');
+    expect(pipe.transform('0.123')).toBe('12.3%');
   });
 
   it('returns null, when input null', () => {

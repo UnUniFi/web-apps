d6ce2b0964bd7f23f01725b806c870c644d4d0dc
hikaruNagamine <59303144+hikaruNagamine@users.noreply.github.com>
2023-02-20 22:56:23 +0900

    refactor: use angular/cdk/dialog (ununifi import wallet with private key form dialog)

--- projects/portal/src/app/models/wallets/wallet.application.service.ts
+++ projects/portal/src/app/models/wallets/wallet.application.service.ts
@@ -254,10 +254,12 @@ export class WalletApplicationService {
   async openUnunifiImportWalletWithPrivateKeyDialog(): Promise<
     (StoredWallet & { mnemonic: string; privateKey: string }) | undefined
   > {
-    const privateWallet: StoredWallet & { mnemonic: string; privateKey: string } = await this.dialog
-      .open(UnunifiImportWalletWithPrivateKeyFormDialogComponent)
-      .afterClosed()
-      .toPromise();
+    const privateWallet: (StoredWallet & { mnemonic: string; privateKey: string }) | undefined =
+      await this.tmp_dialog
+        .open<StoredWallet & { mnemonic: string; privateKey: string }>(
+          UnunifiImportWalletWithPrivateKeyFormDialogComponent,
+        )
+        .closed.toPromise();
     return privateWallet;
   }
 

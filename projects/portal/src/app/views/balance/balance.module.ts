1f727c9f07662120e3abaf9a5342a8777636d424
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-02-25 02:27:50 +0900

    feat: balance fix

--- projects/portal/src/app/views/balance/balance.module.ts
+++ projects/portal/src/app/views/balance/balance.module.ts
@@ -3,10 +3,11 @@ import { ViewBalanceComponent } from './balance.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [ViewBalanceComponent],
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule,PipesModule],
   exports: [ViewBalanceComponent],
 })
 export class ViewBalanceModule {}

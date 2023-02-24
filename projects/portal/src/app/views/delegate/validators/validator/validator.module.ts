dc99b50202d66b229d864bbe6db78cf8860e4e75
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-02-25 02:37:27 +0900

    fix: validator staked

--- projects/portal/src/app/views/delegate/validators/validator/validator.module.ts
+++ projects/portal/src/app/views/delegate/validators/validator/validator.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [ValidatorComponent],
-  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule],
+  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule, PipesModule],
   exports: [ValidatorComponent],
 })
 export class ValidatorModule {}

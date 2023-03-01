799d6b0f7cb744bbba7c523840c717f5221723b5
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-01 19:49:59 +0900

    fix: select market

--- projects/portal/src/app/pages/derivatives/perpetual-futures/perpetual-futures.component.ts
+++ projects/portal/src/app/pages/derivatives/perpetual-futures/perpetual-futures.component.ts
@@ -1,8 +1,11 @@
 import { BankQueryService } from '../../../models/cosmos/bank.query.service';
 import { DerivativesQueryService } from '../../../models/derivatives/derivatives.query.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import cosmosclient from '@cosmos-client/core';
+import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
+import ununificlient from 'ununifi-client';
 
 @Component({
   selector: 'app-perpetual-futures',
@@ -16,8 +19,22 @@ export class PerpetualFuturesComponent implements OnInit {
     .pipe(map((params) => params.perpetual_futures));
 
   denomMetadataMap$ = this.bankQuery.getDenomMetadataMap$();
+  markets$ = combineLatest([this.params$, this.denomMetadataMap$]).pipe(
+    map(([params, metadata]) => params?.markets?.map((market) => this.getMarket(metadata, market))),
+  );
+  selectedMarket$ = this.markets$.pipe(
+    map((markets) => {
+      if (markets && markets.length) {
+        this.onChangeMarket(markets[0]);
+        return markets[0];
+      } else {
+        return undefined;
+      }
+    }),
+  );
 
   constructor(
+    private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly bankQuery: BankQueryService,
     private readonly derivativesQuery: DerivativesQueryService,
@@ -30,5 +47,20 @@ export class PerpetualFuturesComponent implements OnInit {
     const base = market.slice(0, slashIndex);
     const quote = market.slice(slashIndex + 1);
     this.router.navigate(['/derivatives/perpetual-futures', base, quote]);
+  }
+
+  getMarket(
+    denomMetadataMap: {
+      [denom: string]: cosmosclient.proto.cosmos.bank.v1beta1.IMetadata;
+    },
+    market: ununificlient.proto.ununifi.derivatives.IMarket,
+  ) {
+    const baseMetadata = denomMetadataMap?.[market.base_denom || ''];
+    const quoteMetadata = denomMetadataMap?.[market.quote_denom || ''];
+    if (baseMetadata && quoteMetadata) {
+      return baseMetadata.symbol + '/' + quoteMetadata.symbol;
+    } else {
+      return market.base_denom + '/' + market.quote_denom;
+    }
   }
 }
--- projects/portal/src/app/views/derivatives/perpetual-futures/perpetual-futures.component.ts
+++ projects/portal/src/app/views/derivatives/perpetual-futures/perpetual-futures.component.ts
@@ -25,30 +25,20 @@ export class PerpetualFuturesComponent implements OnInit, OnChanges {
   @Input()
   denomMetadataMap?: { [denom: string]: cosmosclient.proto.cosmos.bank.v1beta1.IMetadata } | null;
 
+  @Input()
+  markets?: string[] | null;
+
+  @Input()
+  selectedMarket?: string | null;
+
   @Output()
   changeMarket = new EventEmitter<string>();
-
-  markets?: string[];
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const getMarket = (m: ununificlient.proto.ununifi.derivatives.IMarket) => {
-      const baseMetadata = this.denomMetadataMap?.[m.base_denom || ''];
-      const quoteMetadata = this.denomMetadataMap?.[m.quote_denom || ''];
-      if (baseMetadata && quoteMetadata) {
-        return baseMetadata.symbol + '/' + quoteMetadata.symbol;
-      } else {
-        return m.base_denom + '/' + m.quote_denom;
-      }
-    };
-
-    if (changes.params) {
-      this.markets = this.params?.markets?.map((market) => getMarket(market));
-    }
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 
   onChangeMarket(market: string): void {
     this.changeMarket.emit(market);

5615b8794776063796a75bd599bfa374dc7e3a2b
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-14 18:24:18 +0900

    feat: vaults impl

--- projects/portal/src/app/views/yield-aggregator/vaults/owner/owner.module.ts
+++ projects/portal/src/app/views/yield-aggregator/vaults/owner/owner.module.ts
@@ -1,10 +1,11 @@
 import { OwnerComponent } from './owner.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [OwnerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [OwnerComponent],
 })
 export class OwnerModule {}

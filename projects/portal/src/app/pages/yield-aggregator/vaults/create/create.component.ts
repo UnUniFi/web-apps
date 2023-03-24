bbcf1f42e47004409a35dbb5cd85699edb105e2e
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2023-03-24 23:00:43 +0900

    feat: create vault

--- projects/portal/src/app/pages/yield-aggregator/vaults/create/create.component.ts
+++ projects/portal/src/app/pages/yield-aggregator/vaults/create/create.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { cosmos } from '@cosmos-client/core/esm/proto';
+import { BankQueryService } from 'projects/portal/src/app/models/cosmos/bank.query.service';
 import { YieldAggregatorApplicationService } from 'projects/portal/src/app/models/ununifi/yield-aggregator.application.service';
 import { CreateVaultRequest } from 'projects/portal/src/app/models/ununifi/yield-aggregator.model';
+import { YieldAggregatorQueryService } from 'projects/portal/src/app/models/ununifi/yield-aggregator.query.service';
+import { StoredWallet } from 'projects/portal/src/app/models/wallets/wallet.model';
+import { WalletService } from 'projects/portal/src/app/models/wallets/wallet.service';
+import { Observable } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { StrategyAll200ResponseStrategiesInner } from 'ununifi-client/esm/openapi';
 
 @Component({
   selector: 'app-create',
@@ -8,9 +17,43 @@ import { CreateVaultRequest } from 'projects/portal/src/app/models/ununifi/yield
   styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
-  constructor(private readonly iyaApp: YieldAggregatorApplicationService) {}
+  denom$: Observable<string>;
+  strategies$: Observable<StrategyAll200ResponseStrategiesInner[]>;
+  symbolBalancesMap$: Observable<{ [symbol: string]: number }>;
+  symbolMetadataMap$: Observable<{ [symbol: string]: cosmos.bank.v1beta1.IMetadata }>;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private readonly bankQuery: BankQueryService,
+    private readonly walletService: WalletService,
+    private readonly iyaQuery: YieldAggregatorQueryService,
+    private readonly iyaApp: YieldAggregatorApplicationService,
+  ) {
+    const address$ = this.walletService.currentStoredWallet$.pipe(
+      filter((wallet): wallet is StoredWallet => wallet !== undefined && wallet !== null),
+      map((wallet) => wallet.address),
+    );
+    this.denom$ = this.route.queryParams.pipe(map((params) => params.denom));
+    this.strategies$ = this.denom$.pipe(mergeMap((denom) => this.iyaQuery.listStrategies$(denom)));
+    this.symbolBalancesMap$ = address$.pipe(
+      mergeMap((address) => this.bankQuery.getSymbolBalanceMap$(address)),
+    );
+    this.symbolMetadataMap$ = this.bankQuery.getSymbolMetadataMap$();
+  }
 
   ngOnInit(): void {}
+
+  onChangeDenom(denom: string): void {
+    console.log('denom');
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        denom: denom,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
 
   onCreate(data: CreateVaultRequest) {
     this.iyaApp.createVault(
--- projects/portal/src/app/views/yield-aggregator/vaults/create/create.component.ts
+++ projects/portal/src/app/views/yield-aggregator/vaults/create/create.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { cosmos } from '@cosmos-client/core/esm/proto';
 import { CreateVaultRequest } from 'projects/portal/src/app/models/ununifi/yield-aggregator.model';
+import { StrategyAll200ResponseStrategiesInner } from 'ununifi-client/esm/openapi/api';
 
 @Component({
   selector: 'view-create',
@@ -7,23 +9,58 @@ import { CreateVaultRequest } from 'projects/portal/src/app/models/ununifi/yield
   styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
+  @Input()
+  strategies?: StrategyAll200ResponseStrategiesInner[] | null;
+  @Input()
+  symbolBalancesMap?: { [symbol: string]: number } | null;
+  @Input()
+  symbolMetadataMap?: { [symbol: string]: cosmos.bank.v1beta1.IMetadata } | null;
+  @Output()
+  changeDenom: EventEmitter<string>;
   @Output()
   appCreate: EventEmitter<CreateVaultRequest>;
 
-  firstStrategy = { id: '', distRate: 100 };
-  strategies: { id: string; distRate: number }[] = [];
-  selectedSymbol = 'USDC';
+  name?: string;
+  commissionRate = 0.5;
+  depositAmount = 0.001;
+  feeAmount = 0.0005;
+  firstStrategy = { id: '', weight: 100 };
+  selectedStrategies: { id: string; weight: number }[] = [];
+  selectedSymbol?: string;
 
   constructor() {
+    this.changeDenom = new EventEmitter();
     this.appCreate = new EventEmitter();
   }
 
   ngOnInit(): void {}
 
   onClickAddStrategy() {
-    this.strategies.push({ id: '', distRate: 0 });
+    this.selectedStrategies.push({ id: '', weight: 0 });
   }
   onClickDeleteStrategy(index: number) {
-    this.strategies.splice(index, 1);
+    this.selectedStrategies.splice(index, 1);
+  }
+  onChangeSymbol() {
+    if (!this.selectedSymbol) {
+      return;
+    }
+    const denom = this.symbolMetadataMap?.[this.selectedSymbol].base;
+    if (denom) {
+      this.changeDenom.emit(denom);
+    }
+  }
+  onSubmitCreate() {
+    if (!this.name || !this.selectedSymbol) {
+      return;
+    }
+    this.appCreate.emit({
+      name: this.name,
+      symbol: this.selectedSymbol,
+      strategies: this.selectedStrategies,
+      commissionRate: this.commissionRate,
+      feeAmount: this.feeAmount,
+      depositAmount: this.depositAmount,
+    });
   }
 }

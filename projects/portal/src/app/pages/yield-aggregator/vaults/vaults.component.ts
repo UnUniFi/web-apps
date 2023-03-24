43bd12348f76e76d521732b8b3c58c1b9a704a59
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-24 14:07:08 +0900

    feat: vault list

--- projects/portal/src/app/pages/yield-aggregator/vaults/vaults.component.ts
+++ projects/portal/src/app/pages/yield-aggregator/vaults/vaults.component.ts
@@ -1,15 +1,45 @@
+import { YieldAggregatorQueryService } from '../../../models/ununifi/yield-aggregator.query.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { VaultAll200ResponseVaultsInner } from 'ununifi-client/esm/openapi';
 
 @Component({
   selector: 'app-vaults',
   templateUrl: './vaults.component.html',
-  styleUrls: ['./vaults.component.css']
+  styleUrls: ['./vaults.component.css'],
 })
 export class VaultsComponent implements OnInit {
+  vaults$: Observable<VaultAll200ResponseVaultsInner[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private readonly iyaQuery: YieldAggregatorQueryService,
+  ) {
+    this.vaults$ = combineLatest([this.iyaQuery.listVaults$(), this.route.queryParams]).pipe(
+      map(([vaults, params]) =>
+        params
+          ? vaults.filter((vault) => {
+              const hasIdMatch = vault.id && vault.id.includes(params.search);
+              const hasOwnerMatch = vault.owner && vault.owner.includes(params.search);
+              const hasDenomMatch = vault.denom && vault.denom.includes(params.search);
+              return hasIdMatch || hasOwnerMatch || hasDenomMatch;
+            })
+          : vaults,
+      ),
+    );
   }
 
+  ngOnInit(): void {}
+
+  appSearchValueChanged(value: string): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        search: value,
+      },
+    });
+  }
 }

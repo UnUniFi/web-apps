5f8240d9a4a6ce876de7fb3f815dd3988f4dd622
hikaruNagamine <59303144+hikaruNagamine@users.noreply.github.com>
2023-03-28 00:11:21 +0900

    refactor: use daisy ui (withdraw-incentive-all-rewards-form-dialog)

--- projects/portal/src/app/views/dialogs/incentive/withdraw-incentive-all-rewards-form-dialog/withdraw-incentive-all-rewards-form-dialog.module.ts
+++ projects/portal/src/app/views/dialogs/incentive/withdraw-incentive-all-rewards-form-dialog/withdraw-incentive-all-rewards-form-dialog.module.ts
@@ -1,12 +1,11 @@
-import { MaterialModule } from '../../../material.module';
-import { WithdrawIncentiveAllRewardsFormDialogComponent} from './withdraw-incentive-all-rewards-form-dialog.component';
+import { WithdrawIncentiveAllRewardsFormDialogComponent } from './withdraw-incentive-all-rewards-form-dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [WithdrawIncentiveAllRewardsFormDialogComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [WithdrawIncentiveAllRewardsFormDialogComponent],
 })
-export class WithdrawIncentiveAllRewardsFormDialogModule {}
+export class WithdrawIncentiveAllRewardsFormDialogModule { }

b8742a4ef4522a013dd92d3bb5d3876cd4c55aa6
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-30 13:27:02 +0900

    chore: val name & design

--- projects/portal/src/app/views/nft-pawnshop/lenders/lenders.component.ts
+++ projects/portal/src/app/views/nft-pawnshop/lenders/lenders.component.ts
@@ -43,7 +43,7 @@ export class LendersComponent implements OnInit {
   interestRate: number;
   datePicker: Date;
   isSubmitted: boolean;
-  isOpenedSearch: boolean;
+  isFiltered: boolean;
 
   constructor() {
     this.appSubmit = new EventEmitter();
@@ -58,7 +58,7 @@ export class LendersComponent implements OnInit {
     now.setSeconds(0);
     this.datePicker = now;
     this.isSubmitted = false;
-    this.isOpenedSearch = false;
+    this.isFiltered = false;
   }
 
   ngOnInit(): void {}

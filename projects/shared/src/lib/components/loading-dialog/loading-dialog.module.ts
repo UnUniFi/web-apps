1c11403bfb18c1695b52e01d52534d396954ecfa
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-03-30 19:17:45 +0900

    feat: now using mat-progress-spinner

--- projects/shared/src/lib/components/loading-dialog/loading-dialog.module.ts
+++ projects/shared/src/lib/components/loading-dialog/loading-dialog.module.ts
@@ -2,10 +2,11 @@ import { LoadingDialogComponent } from './loading-dialog.component';
 import { DialogModule } from '@angular/cdk/dialog';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [LoadingDialogComponent],
-  imports: [CommonModule, DialogModule],
+  imports: [CommonModule, DialogModule, MatProgressSpinnerModule],
   exports: [LoadingDialogComponent],
 })
-export class LoadingDialogModule { }
+export class LoadingDialogModule {}

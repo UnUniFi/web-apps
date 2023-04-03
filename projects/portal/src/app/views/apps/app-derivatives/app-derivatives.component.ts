f3244fbe07acddd3cf30aa2b5725820f22e5ebe3
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-03 16:37:31 +0900

    feat: derivetives mobile

--- projects/portal/src/app/views/apps/app-derivatives/app-derivatives.component.ts
+++ projects/portal/src/app/views/apps/app-derivatives/app-derivatives.component.ts
@@ -1,15 +1,17 @@
+import { apps } from '../../tools/app-tool/app-tool.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'view-app-derivatives',
   templateUrl: './app-derivatives.component.html',
-  styleUrls: ['./app-derivatives.component.css']
+  styleUrls: ['./app-derivatives.component.css'],
 })
 export class AppDerivativesComponent implements OnInit {
+  apps: { name: string; link: string; icon: string }[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.apps = apps;
   }
 
+  ngOnInit(): void {}
 }

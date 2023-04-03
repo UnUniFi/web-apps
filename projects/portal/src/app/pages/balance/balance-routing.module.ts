c8d88684475312cf20db33a8e5ee6450e2c7e512
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-03 21:25:06 +0900

    Revert "feat: add new send module"
    This reverts commit 20591166e4ef7e9a05d66fb3403f0145a79e728f.

--- projects/portal/src/app/pages/balance/balance-routing.module.ts
+++ projects/portal/src/app/pages/balance/balance-routing.module.ts
@@ -1,12 +1,8 @@
 import { BalanceComponent } from './balance.component';
-import { SendComponent } from './send/send.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', component: BalanceComponent },
-  { path: 'bank/send', component: SendComponent },
-];
+const routes: Routes = [{ path: '', component: BalanceComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/portal/src/app/pages/balance/balance.module.ts
+++ projects/portal/src/app/pages/balance/balance.module.ts
@@ -1,13 +1,11 @@
 import { ViewBalanceModule } from '../../views/balance/balance.module';
-import { SendModule } from '../../views/balance/send/send.module';
 import { BalanceRoutingModule } from './balance-routing.module';
 import { BalanceComponent } from './balance.component';
-import { SendComponent } from './send/send.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [BalanceComponent, SendComponent],
-  imports: [CommonModule, BalanceRoutingModule, ViewBalanceModule, SendModule],
+  declarations: [BalanceComponent],
+  imports: [CommonModule, BalanceRoutingModule, ViewBalanceModule],
 })
 export class AppBalanceModule {}

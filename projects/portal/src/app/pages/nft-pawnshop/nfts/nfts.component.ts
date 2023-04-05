acd0c3769fcf0e8c635a4b06724ba39f9b67a3ed
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-05 22:32:29 +0900

    feat: impl filter

--- projects/portal/src/app/pages/nft-pawnshop/nfts/nfts.component.ts
+++ projects/portal/src/app/pages/nft-pawnshop/nfts/nfts.component.ts
@@ -1,11 +1,18 @@
 import { NftPawnshopQueryService } from '../../../models/nft-pawnshops/nft-pawnshop.query.service';
 import { NftPawnshopService } from '../../../models/nft-pawnshops/nft-pawnshop.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Metadata } from 'projects/shared/src/lib/models/ununifi/query/nft/nft.model';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { ListedNfts200ResponseListingsInner } from 'ununifi-client/esm/openapi';
 
+export type SearchInfo = {
+  keyword?: string;
+  state?: string;
+  date?: string;
+  time?: string;
+};
 @Component({
   selector: 'app-nfts',
   templateUrl: './nfts.component.html',
@@ -15,12 +22,50 @@ export class NftsComponent implements OnInit {
   listedNfts$: Observable<ListedNfts200ResponseListingsInner[]>;
   nftsMetadata$: Observable<Metadata[]>;
   images$: Observable<string[]>;
+  keyword$: Observable<string>;
+  state$: Observable<string>;
+  date$: Observable<string>;
+  time$: Observable<string>;
+  isSearchBoxOpened$: Observable<boolean>;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private readonly pawnshopService: NftPawnshopService,
     private readonly pawnshopQuery: NftPawnshopQueryService,
   ) {
-    this.listedNfts$ = this.pawnshopQuery.listAllListedNfts$();
+    this.listedNfts$ = combineLatest([
+      this.pawnshopQuery.listAllListedNfts$(),
+      this.route.queryParams,
+    ]).pipe(
+      map(([nfts, params]) => {
+        const keyword = params.keyword;
+        const state = params.state;
+        const date = params.date;
+        const time = params.time;
+        if (keyword) {
+          nfts = nfts.filter(
+            (nft) =>
+              nft.listing?.owner?.includes(keyword) ||
+              nft.listing?.nft_id?.class_id?.includes(keyword) ||
+              nft.listing?.nft_id?.nft_id?.includes(keyword),
+          );
+        }
+        if (state) {
+          nfts = nfts.filter((nft) => nft.listing?.state === state);
+        }
+        if (date) {
+          const searchDate = time ? new Date(date + 'T' + time) : new Date(date);
+          nfts = nfts.filter((nft) => new Date(nft.listing?.started_at!) > searchDate);
+        }
+        return nfts;
+      }),
+    );
+    this.keyword$ = this.route.queryParams.pipe(map((params) => params.keyword));
+    this.state$ = this.route.queryParams.pipe(map((params) => params.state));
+    this.date$ = this.route.queryParams.pipe(map((params) => params.date));
+    this.time$ = this.route.queryParams.pipe(map((params) => params.time));
+    this.isSearchBoxOpened$ = this.route.queryParams.pipe(map((params) => !!params));
     const uris$ = this.listedNfts$.pipe(map((nfts) => nfts.map((nft) => nft.nft_info?.uri)));
     this.nftsMetadata$ = uris$.pipe(
       mergeMap((uris) =>
@@ -41,4 +86,23 @@ export class NftsComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  appSearchNfts(searchInfo: SearchInfo): void {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        keyword: searchInfo.keyword,
+        state: searchInfo.state,
+        date: searchInfo.date,
+        time: searchInfo.time,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  appRefreshPage() {
+    this.router.navigate([], {
+      relativeTo: this.route,
+    });
+  }
 }

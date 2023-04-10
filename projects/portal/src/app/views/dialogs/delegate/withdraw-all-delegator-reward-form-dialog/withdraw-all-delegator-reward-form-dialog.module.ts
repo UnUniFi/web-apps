ffd07a32db830543375fc741f54fa192b930d1ce
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-10 16:59:16 +0900

    feat: mobile close dialog button

--- projects/portal/src/app/views/dialogs/delegate/withdraw-all-delegator-reward-form-dialog/withdraw-all-delegator-reward-form-dialog.module.ts
+++ projects/portal/src/app/views/dialogs/delegate/withdraw-all-delegator-reward-form-dialog/withdraw-all-delegator-reward-form-dialog.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from '../../../material.module';
 import { WithdrawAllDelegatorRewardFormDialogComponent } from './withdraw-all-delegator-reward-form-dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,7 @@ import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [WithdrawAllDelegatorRewardFormDialogComponent],
-  imports: [CommonModule, FormsModule, PipesModule],
+  imports: [CommonModule, FormsModule, PipesModule, MaterialModule],
   exports: [WithdrawAllDelegatorRewardFormDialogComponent],
 })
 export class WithdrawAllDelegatorRewardFormDialogModule {}
--- projects/portal/src/app/views/dialogs/wallets/ununifi/ununifi-backup-mnemonic-and-private-key-wizard-dialog/ununifi-backup-mnemonic-and-private-key-wizard-dialog.module.ts
+++ projects/portal/src/app/views/dialogs/wallets/ununifi/ununifi-backup-mnemonic-and-private-key-wizard-dialog/ununifi-backup-mnemonic-and-private-key-wizard-dialog.module.ts
@@ -1,10 +1,12 @@
+import { MaterialModule } from '../../../../material.module';
 import { UnunifiBackupMnemonicAndPrivateKeyWizardDialogComponent } from './ununifi-backup-mnemonic-and-private-key-wizard-dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [UnunifiBackupMnemonicAndPrivateKeyWizardDialogComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MaterialModule],
   exports: [UnunifiBackupMnemonicAndPrivateKeyWizardDialogComponent],
 })
-export class UnunifiBackupMnemonicAndPrivateKeyWizardDialogModule { }
+export class UnunifiBackupMnemonicAndPrivateKeyWizardDialogModule {}

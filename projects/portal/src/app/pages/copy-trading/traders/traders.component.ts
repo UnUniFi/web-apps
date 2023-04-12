7c0b63108c60e20882f135ecb50ef1fece77cd8a
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-12 16:57:04 +0900

    feat: traders

--- projects/portal/src/app/pages/copy-trading/traders/traders.component.ts
+++ projects/portal/src/app/pages/copy-trading/traders/traders.component.ts
@@ -1,15 +1,41 @@
+import { CopyTradingQueryService } from '../../../models/copy-trading/copy-trading.query.service';
+import { StoredWallet } from '../../../models/wallets/wallet.model';
+import { WalletService } from '../../../models/wallets/wallet.service';
 import { Component, OnInit } from '@angular/core';
+import cosmosclient from '@cosmos-client/core';
+import { Observable } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import {
+  ExemplaryTraderAll200ResponseExemplaryTraderInner,
+  ExemplaryTraderTracing200ResponseTracingInner,
+} from 'ununifi-client/esm/openapi';
 
 @Component({
   selector: 'app-traders',
   templateUrl: './traders.component.html',
-  styleUrls: ['./traders.component.css']
+  styleUrls: ['./traders.component.css'],
 })
 export class TradersComponent implements OnInit {
+  address$: Observable<string>;
+  exemplaryTraders$: Observable<ExemplaryTraderAll200ResponseExemplaryTraderInner[]>;
+  tracing$: Observable<ExemplaryTraderTracing200ResponseTracingInner>;
+  availableTracings$: Observable<ExemplaryTraderTracing200ResponseTracingInner[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly walletService: WalletService,
+    private readonly copyTradingQuery: CopyTradingQueryService,
+  ) {
+    const currentStoredWallet$ = this.walletService.currentStoredWallet$;
+    this.address$ = currentStoredWallet$.pipe(
+      filter((wallet): wallet is StoredWallet => wallet !== undefined && wallet !== null),
+      map((wallet) => cosmosclient.AccAddress.fromString(wallet.address).toString()),
+    );
+    this.tracing$ = this.address$.pipe(
+      mergeMap((address) => this.copyTradingQuery.getTracing$(address)),
+    );
+    this.exemplaryTraders$ = this.copyTradingQuery.listExemplaryTraders$();
+    this.availableTracings$ = this.copyTradingQuery.listAllTracings$();
   }
 
+  ngOnInit(): void {}
 }
--- projects/portal/src/app/views/copy-trading/traders/traders.component.ts
+++ projects/portal/src/app/views/copy-trading/traders/traders.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  ExemplaryTraderAll200ResponseExemplaryTraderInner,
+  ExemplaryTraderTracing200ResponseTracingInner,
+} from 'ununifi-client/esm/openapi';
 
 @Component({
   selector: 'view-traders',
   templateUrl: './traders.component.html',
-  styleUrls: ['./traders.component.css']
+  styleUrls: ['./traders.component.css'],
 })
 export class TradersComponent implements OnInit {
+  @Input() address?: string | null;
+  @Input() tracing?: ExemplaryTraderTracing200ResponseTracingInner | null;
+  @Input() exemplaryTraders?: ExemplaryTraderAll200ResponseExemplaryTraderInner[] | null;
+  @Input() availableTracings?: ExemplaryTraderTracing200ResponseTracingInner[] | null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

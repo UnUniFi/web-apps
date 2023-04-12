2ae5b1247de98ce2ef7826c4afec8cd5b6dd7427
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-04-12 19:30:12 +0900

    feat: trader detail & tx

--- projects/portal/src/app/views/copy-trading/traders/trader/trader.component.ts
+++ projects/portal/src/app/views/copy-trading/traders/trader/trader.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  CreateTracingRequest,
+  UpdateExemplaryTraderRequest,
+} from 'projects/portal/src/app/models/copy-trading/copy-trading.model';
+import {
+  ExemplaryTraderAll200ResponseExemplaryTraderInner,
+  ExemplaryTraderTracing200ResponseTracingInner,
+} from 'ununifi-client/esm/openapi';
 
 @Component({
   selector: 'view-trader',
@@ -6,12 +14,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trader.component.css'],
 })
 export class TraderComponent implements OnInit {
-  sizeCoef: number = 100;
-  leverageCoef: number = 100;
-  commissionRate?: number;
+  @Input() address?: string | null;
+  @Input() exemplaryTraderAddress?: string | null;
+  @Input() trader?: ExemplaryTraderAll200ResponseExemplaryTraderInner | null;
+  @Input() tracing?: ExemplaryTraderTracing200ResponseTracingInner | null;
+  @Input() isTracing?: boolean | null;
+  @Input() userCount?: number | null;
+  @Input() commissionRate?: number | null;
+  @Input() description?: string | null;
+  @Output() createTracing = new EventEmitter<CreateTracingRequest>();
+  @Output() deleteTracing = new EventEmitter<{}>();
+  @Output() updateTrader = new EventEmitter<UpdateExemplaryTraderRequest>();
+  @Output() deleteTrader = new EventEmitter<{}>();
+
+  sizeCoef: number = 1;
+  leverageCoef: number = 1;
+  isReverse: boolean = false;
   newRate?: number;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClickCreateTracing() {
+    if (!this.exemplaryTraderAddress) {
+      return;
+    }
+    this.createTracing.emit({
+      exemplaryTrader: this.exemplaryTraderAddress,
+      sizeCoef: this.sizeCoef,
+      leverageCoef: this.leverageCoef,
+      reverse: this.isReverse,
+    });
+  }
+
+  onClickDeleteTracing() {
+    this.deleteTracing.emit();
+  }
+
+  onClickUpdateTrader() {
+    if (this.newRate === undefined) {
+      return;
+    }
+    this.updateTrader.emit({
+      name: this.trader?.name || '',
+      description: this.description || '',
+      profitCommissionRate: this.newRate,
+    });
+  }
+
+  onClickDeleteTrader() {
+    this.deleteTrader.emit();
+  }
 }

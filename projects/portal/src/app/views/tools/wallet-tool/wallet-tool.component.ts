642a5a7c27b1f9487d2e5bedf4f04acf3b91430d
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-05-22 15:06:54 +0900

    feat: detected change keplr key

--- projects/portal/src/app/views/tools/wallet-tool/wallet-tool.component.ts
+++ projects/portal/src/app/views/tools/wallet-tool/wallet-tool.component.ts
@@ -1,9 +1,14 @@
 import { StoredWallet } from '../../../models/wallets/wallet.model';
 import { Clipboard } from '@angular/cdk/clipboard';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Window as KeplrWindow } from '@keplr-wallet/types';
 import * as crypto from 'crypto';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Window extends KeplrWindow {}
+}
 @Component({
   selector: 'view-wallet-tool',
   templateUrl: './wallet-tool.component.html',
@@ -25,6 +30,12 @@ export class WalletToolComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  @HostListener('window:keplr_keystorechange', ['$event'])
+  onChangeKey() {
+    alert('Key store in Keplr is changed. You may need to refetch the account info.');
+    this.onDisconnectWallet({});
+  }
 
   getColorCode(storedWallet: StoredWallet) {
     const hash = crypto

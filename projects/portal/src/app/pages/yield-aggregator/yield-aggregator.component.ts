e86acaa6f8ddc684576a1af03b3b1542b80885ce
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-06-08 20:07:57 +0900

    feat: iya available asset

--- projects/portal/src/app/pages/yield-aggregator/yield-aggregator.component.ts
+++ projects/portal/src/app/pages/yield-aggregator/yield-aggregator.component.ts
@@ -1,7 +1,9 @@
 import { BankQueryService } from '../../models/cosmos/bank.query.service';
+import { YieldAggregatorQueryService } from '../../models/yield-aggregators/yield-aggregator.query.service';
 import { Component, OnInit } from '@angular/core';
 import { cosmos } from '@cosmos-client/core/esm/proto';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-yield-aggregator',
@@ -10,9 +12,31 @@ import { Observable } from 'rxjs';
 })
 export class YieldAggregatorComponent implements OnInit {
   symbolMetadataMap$: Observable<{ [symbol: string]: cosmos.bank.v1beta1.IMetadata }>;
+  availableSymbols$: Observable<string[]>;
 
-  constructor(private readonly bankQuery: BankQueryService) {
+  constructor(
+    private readonly bankQuery: BankQueryService,
+    private readonly iyaQuery: YieldAggregatorQueryService,
+  ) {
     this.symbolMetadataMap$ = this.bankQuery.getSymbolMetadataMap$();
+    const denomMetadataMap$ = this.bankQuery.getDenomMetadataMap$();
+
+    const allStrategies$ = this.iyaQuery.listStrategies$('');
+    this.availableSymbols$ = combineLatest([allStrategies$, denomMetadataMap$]).pipe(
+      map(([allStrategies, denomMetadataMap]) => {
+        const symbols = allStrategies
+          .map((strategy) => {
+            const denomMetadata = denomMetadataMap[strategy.denom || ''];
+            if (denomMetadata) {
+              return denomMetadata.symbol;
+            } else {
+              return undefined;
+            }
+          })
+          .filter((symbol): symbol is string => typeof symbol == 'string');
+        return [...new Set(symbols)];
+      }),
+    );
   }
 
   ngOnInit(): void {}

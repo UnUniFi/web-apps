270bdf18bb434fedf2a21ebe02a5367b6e6a5b49
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-06-13 18:12:37 +0900

    feat: staking & distribution

--- projects/explorer/src/app/views/accounts/account/distribution/distribution.module.ts
+++ projects/explorer/src/app/views/accounts/account/distribution/distribution.module.ts
@@ -1,10 +1,9 @@
-import { PipesModule } from '../../../../pipes/pipes.module';
 import { MaterialModule } from '../../../material.module';
 import { DistributionComponent } from './distribution.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [DistributionComponent],
--- projects/explorer/src/app/views/accounts/account/staking/staking.module.ts
+++ projects/explorer/src/app/views/accounts/account/staking/staking.module.ts
@@ -1,9 +1,9 @@
-import { PipesModule } from '../../../../pipes/pipes.module';
 import { MaterialModule } from '../../../material.module';
 import { StakingComponent } from './staking.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [StakingComponent],

cd8bce984081dcce389d0c37f48aa10557b41fcd
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-08-11 23:14:06 +0800

    feat: vault list

--- projects/portal/src/app/pages/yieldaggregator/vaults/vaults.component.ts
+++ projects/portal/src/app/pages/yieldaggregator/vaults/vaults.component.ts
@@ -1,9 +1,15 @@
+import {
+  BandProtocolService,
+  TokenAmountUSD,
+} from '../../../models/band-protocols/band-protocol.service';
+import { ConfigService } from '../../../models/config.service';
 import { BankQueryService } from '../../../models/cosmos/bank.query.service';
 import { YieldAggregatorQueryService } from '../../../models/yield-aggregators/yield-aggregator.query.service';
+import { YieldAggregatorService } from '../../../models/yield-aggregators/yield-aggregator.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { first, map, mergeMap } from 'rxjs/operators';
 import { VaultAll200ResponseVaultsInner } from 'ununifi-client/esm/openapi';
 
 @Component({
@@ -14,6 +20,8 @@ import { VaultAll200ResponseVaultsInner } from 'ununifi-client/esm/openapi';
 export class VaultsComponent implements OnInit {
   vaults$: Observable<VaultAll200ResponseVaultsInner[]>;
   symbols$: Observable<{ name: string; img: string }[]>;
+  apy$: Observable<number[]>;
+  totalDeposited$: Observable<TokenAmountUSD[]>;
   keyword$: Observable<string>;
 
   constructor(
@@ -21,6 +29,9 @@ export class VaultsComponent implements OnInit {
     private route: ActivatedRoute,
     private readonly bankQuery: BankQueryService,
     private readonly iyaQuery: YieldAggregatorQueryService,
+    private readonly iyaService: YieldAggregatorService,
+    private readonly configService: ConfigService,
+    private readonly bandProtocolService: BandProtocolService,
   ) {
     this.keyword$ = this.route.queryParams.pipe(map((params) => params.keyword));
     const denomMetadataMap$ = this.bankQuery.getDenomMetadataMap$();
@@ -50,6 +61,50 @@ export class VaultsComponent implements OnInit {
           const img = this.bankQuery.getSymbolImageMap()[symbol];
           return { name: symbol, img: img };
         }),
+      ),
+    );
+    const symbolMetadataMap$ = this.bankQuery.getSymbolMetadataMap$();
+    this.totalDeposited$ = combineLatest([this.symbols$, this.vaults$, symbolMetadataMap$]).pipe(
+      mergeMap(([symbols, vaults, symbolMetadataMap]) =>
+        Promise.all(
+          symbols.map(async (symbol, index) => {
+            const vault = await this.iyaQuery
+              .getVault$(vaults[index].id!)
+              .pipe(first())
+              .toPromise();
+            return this.bandProtocolService.convertToUSDAmount(
+              symbol.name,
+              (
+                Number(vault.total_bonded_amount) +
+                Number(vault.total_unbonding_amount) +
+                Number(vault.total_withdrawal_balance)
+              ).toString(),
+              symbolMetadataMap,
+            );
+          }),
+        ),
+      ),
+    );
+    this.apy$ = combineLatest([this.vaults$, this.configService.config$]).pipe(
+      mergeMap(async ([vaults, config]) =>
+        Promise.all(
+          vaults.map(async (vault) => {
+            // TODO: go to a function
+            // same in vault.component.ts
+            if (!vault.strategy_weights) {
+              return 0;
+            }
+            let vaultAPY = 0;
+            for (const strategyWeight of vault.strategy_weights) {
+              const strategyInfo = config?.strategiesInfo?.find(
+                (strategyInfo) => strategyInfo.id === strategyWeight.strategy_id,
+              );
+              const strategyAPY = await this.iyaService.getStrategyAPR(strategyInfo);
+              vaultAPY += Number(strategyAPY) * Number(strategyWeight.weight);
+            }
+            return vaultAPY;
+          }),
+        ),
       ),
     );
   }

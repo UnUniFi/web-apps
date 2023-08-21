d276924aef4fa6ff60540a3d258aa80229c6330a
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-22 01:09:36 +0900

    feat: value

--- projects/portal/src/app/views/yieldaggregator/vaults/deposit/deposit.module.ts
+++ projects/portal/src/app/views/yieldaggregator/vaults/deposit/deposit.module.ts
@@ -2,10 +2,11 @@ import { DepositComponent } from './deposit.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [DepositComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, PipesModule],
   exports: [DepositComponent],
 })
 export class DepositModule {}

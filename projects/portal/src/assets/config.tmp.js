aeac607bd6d97836572d95bfbe4b12b6f2854e48
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-22 20:39:12 +0900

    fix: alpha config

--- projects/portal/src/assets/config.tmp.js
+++ projects/portal/src/assets/config.tmp.js
@@ -42,7 +42,7 @@ const messageModules = [
 
 const apps = [
   { name: 'Utilities', link: '/', icon: 'assistant' },
-  // { name: 'NFT Backed Loan', link: '/nft-backed-loan', icon: 'loyalty' },
+  { name: 'NFT Backed Loan', link: '/nft-backed-loan', icon: 'loyalty' },
   { name: 'Yield Aggregator', link: '/yield-aggregator/vaults', icon: 'pie_chart' },
   // { name: 'Derivatives', link: '/derivatives/perpetual-futures', icon: 'show_chart' },
 ];

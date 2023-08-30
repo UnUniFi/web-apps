f9a066f45d512e2a7089ea69b2aaa20222b83fa7
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-08-30 22:49:35 +0800

    feat: kyc

--- projects/cefi/src/app/kyc/kyc.component.ts
+++ projects/cefi/src/app/kyc/kyc.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-kyc',
   templateUrl: './kyc.component.html',
-  styleUrls: ['./kyc.component.css']
+  styleUrls: ['./kyc.component.css'],
 })
 export class KycComponent implements OnInit {
+  address$ = this.route.paramMap.pipe(map((params) => params.get('address')));
 
-  constructor() { }
+  email = '';
+  name = '';
+  country = '';
+  zipcode = '';
+  address = '';
+  birthday = '';
 
-  ngOnInit(): void {
+  processing = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.processing = true;
+
+    this.processing = false;
   }
-
 }
--- projects/cefi/src/app/kyc/kyc.module.ts
+++ projects/cefi/src/app/kyc/kyc.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { AdminComponent } from './admin/admin.component';
 import { KycRoutingModule } from './kyc-routing.module';
 import { KycComponent } from './kyc.component';
-import { AdminComponent } from './admin/admin.component';
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    KycComponent,
-    AdminComponent
-  ],
-  imports: [
-    CommonModule,
-    KycRoutingModule
-  ]
+  declarations: [KycComponent, AdminComponent],
+  imports: [CommonModule, KycRoutingModule, FormsModule],
 })
-export class KycModule { }
+export class KycModule {}

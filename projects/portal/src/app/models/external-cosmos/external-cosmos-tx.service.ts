52f00afbfdc34e46a317aaa3772c48d29d8d3b85
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-06 18:20:11 +0900

    feat: external cosmos

---
+++ projects/portal/src/app/models/external-cosmos/external-cosmos-tx.service.ts
@@ -0,0 +1,163 @@
+import {
+  convertHexStringToUint8Array,
+  convertUnknownAccountToBaseAccount,
+} from '../../utils/converter';
+import { BankService } from '../cosmos/bank.service';
+import { KeplrService } from '../wallets/keplr/keplr.service';
+import { LeapService } from '../wallets/leap/leap.service';
+import { CosmosWallet, WalletType } from '../wallets/wallet.model';
+import { ExternalCosmosSdkService } from './external-cosmos-sdk.service';
+import { Injectable } from '@angular/core';
+import cosmosclient from '@cosmos-client/core';
+import cosmwasmclient from '@cosmos-client/cosmwasm';
+import Long from 'long';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExternalCosmosTxService {
+  constructor(
+    private readonly bankService: BankService,
+    private readonly keplrService: KeplrService,
+    private readonly leapService: LeapService,
+    private readonly cosmwasmSdkService: ExternalCosmosSdkService,
+  ) {}
+
+  buildMsgExecuteContract(
+    sender: string,
+    contractAddress: string,
+    msg: any,
+    amounts: { denom: string; readableAmount: number }[],
+  ) {
+    const coins = amounts.map(
+      (amount) =>
+        this.bankService.convertDenomReadableAmountMapToCoins({
+          [amount.denom]: amount.readableAmount,
+        })[0],
+    );
+    const msgString = JSON.stringify(msg);
+    const msgUint8Array = convertHexStringToUint8Array(msgString);
+    const ExecuteMsg = new cosmwasmclient.proto.cosmwasm.wasm.v1.MsgExecuteContract({
+      sender,
+      contract: contractAddress,
+      msg: msgUint8Array,
+      funds: coins,
+    });
+    return ExecuteMsg;
+  }
+
+  async buildTxBuilder(
+    id: string,
+    messages: any[],
+    cosmosPublicKey: cosmosclient.PubKey,
+    baseAccount: cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount,
+    gas?: cosmosclient.proto.cosmos.base.v1beta1.ICoin,
+    fee?: cosmosclient.proto.cosmos.base.v1beta1.ICoin,
+  ): Promise<cosmosclient.TxBuilder> {
+    const sdk = await this.cosmwasmSdkService.sdk(id).then((sdk) => sdk.rest);
+    const packedAnyMessages: cosmosclient.proto.google.protobuf.IAny[] = messages.map((message) =>
+      cosmosclient.codec.instanceToProtoAny(message),
+    );
+    const txBody = new cosmosclient.proto.cosmos.tx.v1beta1.TxBody({
+      messages: packedAnyMessages,
+    });
+
+    const authInfo = new cosmosclient.proto.cosmos.tx.v1beta1.AuthInfo({
+      signer_infos: [
+        {
+          public_key: cosmosclient.codec.instanceToProtoAny(cosmosPublicKey),
+          mode_info: {
+            single: {
+              mode: cosmosclient.proto.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT,
+            },
+          },
+          sequence: baseAccount.sequence,
+        },
+      ],
+      fee: {
+        amount: fee?.amount && fee.amount !== '0' ? [fee] : [],
+        gas_limit: Long.fromString(gas?.amount ? gas.amount : '1000000'),
+      },
+    });
+    const txBuilder = new cosmosclient.TxBuilder(sdk, txBody, authInfo);
+    return txBuilder;
+  }
+
+  async getBaseAccount(
+    id: string,
+    cosmosPublicKey: cosmosclient.PubKey,
+  ): Promise<cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount | null | undefined> {
+    const sdk = await this.cosmwasmSdkService.sdk(id).then((sdk) => sdk.rest);
+    const accAddress = cosmosclient.AccAddress.fromPublicKey(cosmosPublicKey);
+    const account = await cosmosclient.rest.auth
+      .account(sdk, accAddress)
+      .then((res) =>
+        cosmosclient.codec.protoJSONToInstance(
+          cosmosclient.codec.castProtoJSONOfProtoAny(res.data?.account),
+        ),
+      )
+      .catch((_) => undefined);
+    const baseAccount = convertUnknownAccountToBaseAccount(account);
+    if (!baseAccount) {
+      throw Error('Unused Account or Unsupported Account Type!');
+    }
+    return baseAccount;
+  }
+
+  async getBaseAccountFromAddress(
+    id: string,
+    address: cosmosclient.AccAddress,
+  ): Promise<cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount | null | undefined> {
+    const sdk = await this.cosmwasmSdkService.sdk(id).then((sdk) => sdk.rest);
+    const account = await cosmosclient.rest.auth
+      .account(sdk, address)
+      .then((res) =>
+        cosmosclient.codec.protoJSONToInstance(
+          cosmosclient.codec.castProtoJSONOfProtoAny(res.data?.account),
+        ),
+      )
+      .catch((_) => undefined);
+    const baseAccount = convertUnknownAccountToBaseAccount(account);
+    if (!baseAccount) {
+      throw Error('Unused Account or Unsupported Account Type!');
+    }
+    return baseAccount;
+  }
+
+  async signTx(
+    id: string,
+    txBuilder: cosmosclient.TxBuilder,
+    signerBaseAccount: cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount,
+    currentCosmosWallet: CosmosWallet,
+  ): Promise<cosmosclient.TxBuilder | undefined> {
+    if (currentCosmosWallet.type === WalletType.keplr) {
+      return await this.signTxWithKeplr(id, txBuilder, signerBaseAccount);
+    }
+    if (currentCosmosWallet.type === WalletType.leap) {
+      return await this.signTxWithLeap(id, txBuilder, signerBaseAccount);
+    }
+    throw Error('Unsupported wallet type!');
+  }
+
+  async signTxWithKeplr(
+    id: string,
+    txBuilder: cosmosclient.TxBuilder,
+    signerBaseAccount: cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount,
+  ): Promise<cosmosclient.TxBuilder> {
+    const signedTxBuilder = await this.keplrService.signTxExternal(
+      id,
+      txBuilder,
+      signerBaseAccount,
+    );
+    return signedTxBuilder;
+  }
+
+  async signTxWithLeap(
+    id: string,
+    txBuilder: cosmosclient.TxBuilder,
+    signerBaseAccount: cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount,
+  ): Promise<cosmosclient.TxBuilder> {
+    const signedTxBuilder = await this.leapService.signTxExternal(id, txBuilder, signerBaseAccount);
+    return signedTxBuilder;
+  }
+}
---
+++ projects/portal/src/app/models/external-cosmos/external-cosmos.application.service.ts
@@ -0,0 +1,68 @@
+import { ExternalCosmosTxService } from './external-cosmos-tx.service';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import cosmosclient from '@cosmos-client/core';
+import { BroadcastTx200Response } from '@cosmos-client/core/esm/openapi';
+import { LoadingDialogService } from 'projects/shared/src/lib/components/loading-dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExternalCosmosApplicationService {
+  constructor(
+    private readonly externalCosmosTxService: ExternalCosmosTxService,
+    private readonly loadingDialog: LoadingDialogService,
+    private readonly snackBar: MatSnackBar,
+  ) {}
+
+  async broadcast(
+    id: string,
+    msg: any,
+    cosmosPublicKey: cosmosclient.PubKey,
+    baseAccount: cosmosclient.proto.cosmos.auth.v1beta1.BaseAccount,
+  ) {
+    const dialogRef = this.loadingDialog.open('Sending');
+
+    let txResult: BroadcastTx200Response | undefined;
+    let txHash: string | undefined;
+
+    try {
+      const txBuilder = await this.externalCosmosTxService.buildTxBuilder(
+        id,
+        [msg], // TODO
+        cosmosPublicKey,
+        baseAccount,
+      );
+
+      const signerBaseAccount = await this.externalCosmosTxService.getBaseAccount(
+        id,
+        cosmosPublicKey,
+      );
+      if (!signerBaseAccount) {
+        throw Error('Unsupported Account!');
+      }
+      const signedTxBuilder = await this.externalCosmosTxService.signTx(
+        id,
+        txBuilder,
+        signerBaseAccount,
+        currentCosmosWallet,
+      );
+      if (!signedTxBuilder) {
+        throw Error('Failed to sign!');
+      }
+
+      txResult = await this.externalCosmosTxService.announceTx(txBuilder);
+      txHash = txResult?.tx_response?.txhash;
+      if (txHash === undefined) {
+        throw Error(txResult?.tx_response?.raw_log);
+      }
+      return txHash;
+    } catch (error) {
+      console.error(error);
+      this.snackBar.open(`Tx broadcasting failed: ${(error as Error).toString()}`, 'Close');
+      return null;
+    } finally {
+      dialogRef.close();
+    }
+  }
+}

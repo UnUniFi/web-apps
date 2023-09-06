fcb2e0f9a9187d71dfa31b102760ae8a9ab93fa6
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-06 16:15:06 +0900

    feat: rename

---
+++ projects/portal/src/app/models/cosmwasm/cosmwasm-sdk.service.ts
@@ -0,0 +1,42 @@
+import { ChainInfo, Config, ConfigService } from '../config.service';
+import { Injectable } from '@angular/core';
+import cosmosclient from '@cosmos-client/core';
+import { first, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CosmoWasmSdkService {
+  constructor(private readonly configS: ConfigService) {}
+
+  async sdk(
+    id: string,
+  ): Promise<{ rest: cosmosclient.CosmosSDK; websocket: cosmosclient.CosmosSDK }> {
+    const chain$ = this.configS.config$.pipe(
+      map((config) => config?.externalChains.find((chain) => chain.id === id)),
+    );
+    const sdk$ = chain$.pipe(
+      map((chain) => {
+        if (!chain) {
+          throw new Error('chain not found');
+        }
+        return {
+          rest: new cosmosclient.CosmosSDK(chain.rest, chain.chainId),
+          websocket: new cosmosclient.CosmosSDK(chain.rpc, chain.chainId),
+        };
+      }),
+    );
+    return sdk$.pipe(first()).toPromise();
+  }
+
+  setBech32PrefixToCosmosclient(chain: ChainInfo) {
+    cosmosclient.config.setBech32Prefix({
+      accAddr: chain.bech32Config.bech32PrefixAccAddr,
+      accPub: chain.bech32Config.bech32PrefixAccPub,
+      valAddr: chain.bech32Config.bech32PrefixValAddr,
+      valPub: chain.bech32Config.bech32PrefixValPub,
+      consAddr: chain.bech32Config.bech32PrefixConsAddr,
+      consPub: chain.bech32Config.bech32PrefixConsPub,
+    });
+  }
+}

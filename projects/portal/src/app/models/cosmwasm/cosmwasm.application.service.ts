802e5ae14a2cd551f9de2f0323b98cfcb92beab3
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-07 00:50:04 +0900

    feat: cosmwasm

--- projects/portal/src/app/models/cosmwasm/cosmwasm.application.service.ts
+++ projects/portal/src/app/models/cosmwasm/cosmwasm.application.service.ts
@@ -2,6 +2,7 @@ import {
   TxConfirmDialogComponent,
   TxConfirmDialogData,
 } from '../../views/dialogs/txs/tx-confirm/tx-confirm-dialog.component';
+import { TxCommonApplicationService } from '../cosmos/tx-common.application.service';
 import { ExternalCosmosApplicationService } from '../external-cosmos/external-cosmos.application.service';
 import { CosmwasmService } from './cosmwasm.service';
 import { Dialog } from '@angular/cdk/dialog';
@@ -13,6 +14,7 @@ import { Injectable } from '@angular/core';
 export class CosmoWasmApplicationService {
   constructor(
     private readonly externalCosmosAppService: ExternalCosmosApplicationService,
+    private readonly txCommonApplication: TxCommonApplicationService,
     private readonly cosmwasmService: CosmwasmService,
     private readonly dialog: Dialog,
   ) {}
@@ -23,6 +25,12 @@ export class CosmoWasmApplicationService {
     contractMsg: any,
     amounts: { readableAmount: number; denom: string }[],
   ) {
+    const prerequisiteData = await this.txCommonApplication.getPrerequisiteData();
+    if (!prerequisiteData) {
+      return;
+    }
+    const { address, publicKey, account, currentCosmosWallet, minimumGasPrice } = prerequisiteData;
+
     const msg = this.cosmwasmService.buildMsgExecuteContract(
       sender,
       contractAddress,
@@ -30,10 +38,33 @@ export class CosmoWasmApplicationService {
       amounts,
     );
 
-    const txHash = await this.externalCosmosAppService.broadcast(msg);
+    const simulationResult = await this.txCommonApplication.simulate(
+      msg,
+      publicKey,
+      account,
+      minimumGasPrice,
+    );
+    if (!simulationResult) {
+      return;
+    }
+    const { gas, fee } = simulationResult;
+
+    if (!(await this.txCommonApplication.confirmFeeIfUnUniFiWallet(currentCosmosWallet, fee))) {
+      return;
+    }
+
+    const txHash = await this.txCommonApplication.broadcast(
+      msg,
+      currentCosmosWallet,
+      publicKey,
+      account,
+      gas,
+      fee,
+    );
     if (!txHash) {
       return;
     }
+
     await this.dialog
       .open<TxConfirmDialogData>(TxConfirmDialogComponent, {
         data: { txHash: txHash, msg: 'Deposit to the vault was successful.' },

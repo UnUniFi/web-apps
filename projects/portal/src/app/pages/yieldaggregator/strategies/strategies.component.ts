4f33e1eff6322cf2ac643c3ef6a9a8f284cb0f27
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-07 20:45:48 +0900

    feat: strategies list page

--- projects/portal/src/app/pages/yieldaggregator/strategies/strategies.component.ts
+++ projects/portal/src/app/pages/yieldaggregator/strategies/strategies.component.ts
@@ -14,14 +14,11 @@ import { StrategyAll200ResponseStrategiesInner } from 'ununifi-client/esm/openap
 })
 export class StrategiesComponent implements OnInit {
   denom$: Observable<string>;
-  ibcDenom$: Observable<string>;
-  symbol$: Observable<string>;
-  displaySymbol$: Observable<string>;
-  availableSymbols$: Observable<{ symbol: string; display: string }[]>;
-  symbolMetadataMap$: Observable<{
-    [symbol: string]: cosmosclient.proto.cosmos.bank.v1beta1.IMetadata;
+  denomMetadataMap$: Observable<{
+    [denom: string]: cosmosclient.proto.cosmos.bank.v1beta1.IMetadata;
   }>;
-  symbolImage$: Observable<string | null>;
+  symbolImageMap: { [symbol: string]: string };
+  availableDenoms$: Observable<string[]>;
   strategies$: Observable<StrategyAll200ResponseStrategiesInner[]>;
 
   constructor(
@@ -31,57 +28,23 @@ export class StrategiesComponent implements OnInit {
     private readonly iyaQuery: YieldAggregatorQueryService,
   ) {
     this.denom$ = this.route.params.pipe(map((params) => params.denom));
-    this.ibcDenom$ = this.route.params.pipe(
-      map((params) => (params.ibc_denom ? 'ibc/' + params.ibc_denom : '')),
-    );
-    this.symbolMetadataMap$ = this.bankQuery.getSymbolMetadataMap$();
-    const denomMetadataMap$ = this.bankQuery.getDenomMetadataMap$();
-    this.symbol$ = combineLatest([this.denom$, this.ibcDenom$, denomMetadataMap$]).pipe(
-      map(([denom, ibcDenom, denomMetadataMap]) =>
-        ibcDenom == ''
-          ? denomMetadataMap[denom]?.symbol || ''
-          : denomMetadataMap[ibcDenom]?.symbol || '',
-      ),
-    );
-    this.displaySymbol$ = combineLatest([this.denom$, this.ibcDenom$, denomMetadataMap$]).pipe(
-      map(([denom, ibcDenom, denomMetadataMap]) =>
-        ibcDenom == ''
-          ? denomMetadataMap[denom]?.display || denom
-          : denomMetadataMap[ibcDenom]?.display || denom,
-      ),
-    );
-    this.symbolImage$ = this.symbol$.pipe(
-      map((symbol) => (symbol ? this.bankQuery.getSymbolImageMap()[symbol] : null)),
-    );
+    this.denomMetadataMap$ = this.bankQuery.getDenomMetadataMap$();
+    this.symbolImageMap = this.bankQuery.getSymbolImageMap();
     const allStrategies$ = this.iyaQuery.listStrategies$();
-    this.availableSymbols$ = combineLatest([allStrategies$, denomMetadataMap$]).pipe(
-      map(([allStrategies, denomMetadataMap]) => {
-        const symbols = allStrategies
-          .map((strategy) => {
-            const denomMetadata = denomMetadataMap[strategy.strategy?.denom || ''];
-            if (denomMetadata) {
-              return {
-                symbol: denomMetadata.symbol || '',
-                display: denomMetadata.display || strategy.strategy?.denom || '',
-              };
-            } else {
-              return undefined;
-            }
-          })
-          .filter((symbol): symbol is { symbol: string; display: string } => !!symbol);
-        return [...new Set(symbols)];
+    this.availableDenoms$ = allStrategies$.pipe(
+      map((allStrategies) => {
+        const denoms = allStrategies
+          .map((strategy) => strategy.strategy?.denom)
+          .filter((denom): denom is string => !!denom);
+        return [...new Set(denoms)];
       }),
     );
-    this.strategies$ = combineLatest([allStrategies$, this.denom$, this.ibcDenom$]).pipe(
-      map(([strategies, denom, ibcDenom]) => {
-        if (ibcDenom == '') {
-          if (denom) {
-            return strategies.filter((strategy) => strategy.strategy?.denom == denom);
-          } else {
-            return strategies;
-          }
+    this.strategies$ = combineLatest([allStrategies$, this.denom$]).pipe(
+      map(([strategies, denom]) => {
+        if (denom) {
+          return strategies.filter((strategy) => strategy.strategy?.denom == denom);
         } else {
-          return strategies.filter((strategy) => strategy.strategy?.denom == ibcDenom);
+          return strategies;
         }
       }),
     );

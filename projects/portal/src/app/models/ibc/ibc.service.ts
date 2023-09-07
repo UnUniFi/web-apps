ead36c2d2642f0eac24133df4481849c899c6108
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-07 15:40:18 +0900

    feat: ibc service

---
+++ projects/portal/src/app/models/ibc/ibc.service.ts
@@ -0,0 +1,57 @@
+import { BankService } from '../cosmos/bank.service';
+import { Injectable } from '@angular/core';
+import cosmosclient from '@cosmos-client/core';
+import ibcclient from '@cosmos-client/ibc';
+import Long from 'long';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IbcService {
+  constructor(private readonly bankService: BankService) {}
+
+  buildMsgExecuteContract(
+    sourcePort: string,
+    sourceChannel: string,
+    sender: string,
+    receiver: string,
+    memo: any,
+    timeoutTimestamp: number,
+    timeoutHeight?: {
+      revisionNumber: number;
+      revisionHeight: number;
+    },
+    amount?: { denom: string; readableAmount: number },
+  ) {
+    let coin: cosmosclient.proto.cosmos.base.v1beta1.ICoin | undefined;
+    if (!amount) {
+      coin = undefined;
+    } else {
+      coin = this.bankService.convertDenomReadableAmountMapToCoins({
+        [amount.denom]: amount.readableAmount,
+      })[0];
+    }
+    let IHeightTimeout: ibcclient.ibcproto.ibc.core.client.v1.IHeight | undefined;
+    if (!timeoutHeight) {
+      IHeightTimeout = undefined;
+    } else {
+      IHeightTimeout = {
+        revision_number: Long.fromNumber(timeoutHeight.revisionNumber),
+        revision_height: Long.fromNumber(timeoutHeight.revisionHeight),
+      };
+    }
+
+    const memoString = JSON.stringify(memo);
+    const ExecuteMsg = new ibcclient.ibcproto.ibc.applications.transfer.v1.MsgTransfer({
+      source_port: sourcePort,
+      source_channel: sourceChannel,
+      token: coin,
+      sender,
+      receiver,
+      timeout_height: IHeightTimeout,
+      timeout_timestamp: Long.fromNumber(timeoutTimestamp),
+      memo: memoString,
+    });
+    return ExecuteMsg;
+  }
+}

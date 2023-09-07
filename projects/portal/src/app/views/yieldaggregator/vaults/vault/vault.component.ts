adecf771a91dfb082042a5c0203287411aa4a8be
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-07 15:02:17 +0900

    fix: select withdraw option

--- projects/portal/src/app/views/yieldaggregator/vaults/vault/vault.component.ts
+++ projects/portal/src/app/views/yieldaggregator/vaults/vault/vault.component.ts
@@ -82,8 +82,19 @@ export class VaultComponent implements OnInit, OnChanges {
     external: false,
     cosmos: true,
   };
-  withUnbondingPeriod = false;
-
+  withdrawOptions = [
+    {
+      id: 'immediate',
+      display: 'Immediate withdrawal',
+      disabled: false,
+    },
+    {
+      id: 'unbonding',
+      display: 'Withdrawal after the unbonding period (coming soon)',
+      disabled: true,
+    },
+  ];
+  withdrawOption = this.withdrawOptions[0];
   chains: ExternalChain[] = [
     {
       id: 'ununifi',
@@ -156,6 +167,7 @@ export class VaultComponent implements OnInit, OnChanges {
     this.changeWithdraw = new EventEmitter();
     this.appWithdraw = new EventEmitter();
     this.appClickChain = new EventEmitter();
+    this.withdrawOption = this.withdrawOptions[0];
   }
 
   ngOnInit(): void {}

44946e5ddcdadf6ab729ff1133ff5f276ca60f5d
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-09-08 15:38:34 +0800

    feat: tailwind

---
+++ projects/kyc-provider/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import Header from './Header.svelte';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>

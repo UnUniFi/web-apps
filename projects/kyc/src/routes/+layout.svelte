cb3403e699a88c43bb6fb6145221a1fdef5cf166
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-09-08 16:59:14 +0800

    feat: kyc

---
+++ projects/kyc/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+	import Header from './Header.svelte';
+</script>
+
+<div class="app">
+  <script src="static/config.js"></script>
+
+	<Header />
+
+	<main class="max-w-xl mx-auto">
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>

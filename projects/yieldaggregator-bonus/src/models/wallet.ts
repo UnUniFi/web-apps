a66eb8e90abaefd5cf2dfc09d8e8eaac3fe4f029
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-09-10 18:24:29 +0800

    feat: cefi

---
+++ projects/yieldaggregator-bonus/src/models/wallet.ts
@@ -0,0 +1,4 @@
+import { wallets } from 'cosmos-kit';
+
+wallets.keplr[0].activate();
+wallets.leap[0].activate();
--- projects/yieldaggregator-bonus/src/routes/bonus-windows/[bonusWindowId]/Stake.svelte
+++ projects/yieldaggregator-bonus/src/routes/bonus-windows/[bonusWindowId]/Stake.svelte
@@ -2,6 +2,7 @@
 	export let bonusWindowId: string;
 
 	import { formula } from 'svelte-formula';
+	import type { VaultShareStaking } from '../../../models/YieldaggregatorBonus.types';
 
 	let processing = false;
 	let vaultId = 0;
@@ -15,7 +16,15 @@
 		formValidators
 	});
 
-	const alreadyStaked = false;
+	$: {
+		vaultShareStaking = new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ bonusWindowId } as any);
+			}, 1000);
+		}) as Promise<VaultShareStaking | undefined>;
+
+		alreadyStaked = vaultShareStaking.then((v) => v !== undefined);
+	}
 
 	function stake() {}
 </script>
@@ -25,80 +34,82 @@
 		<div class="card-body">
 			<h2 class="card-title">Stake your Vault Share Token</h2>
 
-			{#if alreadyStaked}
-				<p>You have already staked in this bonus window</p>
-			{:else}
-				<form on:submit={stake}>
-					<div class="form-control w-full">
-						<span class="label">
-							<span class="label-text">Bonus Window ID</span>
-							<span class="label-text-alt" />
-						</span>
-						<input
-							type="text"
-							placeholder=""
-							class="input input-bordered w-full"
-							required
-							readonly
-							bind:value={bonusWindowId}
-							name="bonusWindowId"
-						/>
-						<span class="label">
-							<span class="label-text" />
-							<span class="label-text-alt" />
-						</span>
-					</div>
-					<div class="form-control w-full">
-						<span class="label">
-							<span class="label-text">Vault ID</span>
-							<span class="label-text-alt" />
-						</span>
-						<input
-							type="number"
-							placeholder=""
-							class="input input-bordered w-full"
-							class:input-error={$touched.vaultId && !$validity.vaultId}
-							required
-							readonly={processing}
-							bind:value={vaultId}
-							name="vaultId"
-						/>
-						<span class="label">
-							<span class="label-text" />
-							<span class="label-text-alt" />
-						</span>
-					</div>
-					<div class="form-control w-full">
-						<span class="label">
-							<span class="label-text">Share Amount</span>
-							<span class="label-text-alt" />
-						</span>
-						<div class="join">
+			{#await alreadyStaked}
+				{#if alreadyStaked}
+					<p>You have already staked in this bonus window</p>
+				{:else}
+					<form on:submit={stake}>
+						<div class="form-control w-full">
+							<span class="label">
+								<span class="label-text">Bonus Window ID</span>
+								<span class="label-text-alt" />
+							</span>
+							<input
+								type="text"
+								placeholder=""
+								class="input input-bordered w-full"
+								required
+								readonly
+								bind:value={bonusWindowId}
+								name="bonusWindowId"
+							/>
+							<span class="label">
+								<span class="label-text" />
+								<span class="label-text-alt" />
+							</span>
+						</div>
+						<div class="form-control w-full">
+							<span class="label">
+								<span class="label-text">Vault ID</span>
+								<span class="label-text-alt" />
+							</span>
 							<input
 								type="number"
 								placeholder=""
-								class="join-item input input-bordered w-full"
-								class:input-error={$touched.shareAmount && !$validity.shareAmount}
+								class="input input-bordered w-full"
+								class:input-error={$touched.vaultId && !$validity.vaultId}
 								required
 								readonly={processing}
-								bind:value={shareAmount}
-								name="shareAmount"
+								bind:value={vaultId}
+								name="vaultId"
 							/>
-							<button disabled class="join-item btn">YA-SHARE-</button>
+							<span class="label">
+								<span class="label-text" />
+								<span class="label-text-alt" />
+							</span>
 						</div>
+						<div class="form-control w-full">
+							<span class="label">
+								<span class="label-text">Share Amount</span>
+								<span class="label-text-alt" />
+							</span>
+							<div class="join">
+								<input
+									type="number"
+									placeholder=""
+									class="join-item input input-bordered w-full"
+									class:input-error={$touched.shareAmount && !$validity.shareAmount}
+									required
+									readonly={processing}
+									bind:value={shareAmount}
+									name="shareAmount"
+								/>
+								<button disabled class="join-item btn">YA-SHARE-</button>
+							</div>
 
-						<span class="label">
-							<span class="label-text" />
-							<span class="label-text-alt" />
-						</span>
-					</div>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary" disabled={!$isFormValid || processing}>
-							Sign In
-						</button>
-					</div>
-				</form>
-			{/if}
+							<span class="label">
+								<span class="label-text" />
+								<span class="label-text-alt" />
+							</span>
+						</div>
+						<div class="card-actions justify-end">
+							<button class="btn btn-primary" disabled={!$isFormValid || processing}>
+								Sign In
+							</button>
+						</div>
+					</form>
+				{/if}
+			{/await}
 		</div>
 	</div>
 </section>

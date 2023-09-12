8eae2829d56a68f22e9d68c44c8672bfce005d5b
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-12 18:14:10 +0900

    Revert "feat: wallet app"
    This reverts commit fa6cc8f6b40ef86692807618d1ebae57253c2322.

--- projects/portal/src/app/models/wallets/wallet.application.service.ts
+++ projects/portal/src/app/models/wallets/wallet.application.service.ts
@@ -18,7 +18,6 @@ import { LeapService } from './leap/leap.service';
 import { MetaMaskService } from './metamask/metamask.service';
 import { WalletType, StoredWallet, ExternalWallet } from './wallet.model';
 import { WalletService } from './wallet.service';
-import { Web3ModalService } from './web3modal/web3modal.service';
 import { Dialog } from '@angular/cdk/dialog';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -34,7 +33,6 @@ export class WalletApplicationService {
     private readonly keplrService: KeplrService,
     private readonly leapService: LeapService,
     private readonly metaMaskService: MetaMaskService,
-    private readonly web3ModalService: Web3ModalService,
     private readonly dialog: Dialog,
     private snackBar: MatSnackBar,
     private loadingDialog: LoadingDialogService,
@@ -206,12 +204,9 @@ export class WalletApplicationService {
           const address = await this.metaMaskService.getEthAddress();
           return { walletType: WalletType.metamask, address: address };
         case WalletType.walletConnect:
-          const wallet = this.web3ModalService.getEvmWalletManager();
-          console.log(wallet);
-          return {
-            walletType: WalletType.walletConnect,
-            address: wallet.ethereumClient.getAccount().address,
-          };
+          // return await this.connectExternalWallet(this.walletConnectService);
+          this.snackBar.open('WalletConnect is not supported yet.', 'Close');
+          return;
         default:
           return;
       }

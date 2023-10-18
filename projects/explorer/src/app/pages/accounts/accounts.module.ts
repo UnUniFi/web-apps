afd6da63df0a22d26dad582e0f85a88b975cd72c
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-10-18 21:09:36 +0900

    feat: routing

--- projects/explorer/src/app/pages/accounts/accounts.module.ts
+++ projects/explorer/src/app/pages/accounts/accounts.module.ts
@@ -2,16 +2,27 @@ import { AccountModule } from '../../views/accounts/account/account.module';
 import { DistributionModule } from '../../views/accounts/account/distribution/distribution.module';
 import { StakingModule } from '../../views/accounts/account/staking/staking.module';
 import { TxsModule } from '../../views/accounts/account/txs/txs.module';
+import { AddressModule } from '../../views/accounts/yieldaggregator/address/address.module';
+import { YieldaggregatorModule } from '../../views/accounts/yieldaggregator/yieldaggregator.module';
 import { AccountComponent } from './account/account.component';
 import { DistributionComponent } from './account/distribution/distribution.component';
 import { StakingComponent } from './account/staking/staking.component';
 import { TxsComponent } from './account/txs/txs.component';
 import { AccountsRoutingModule } from './accounts-routing.module';
+import { AddressComponent } from './yieldaggregator/address/address.component';
+import { YieldaggregatorComponent } from './yieldaggregator/yieldaggregator.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AccountComponent, StakingComponent, DistributionComponent, TxsComponent],
+  declarations: [
+    AccountComponent,
+    StakingComponent,
+    DistributionComponent,
+    TxsComponent,
+    YieldaggregatorComponent,
+    AddressComponent,
+  ],
   imports: [
     CommonModule,
     AccountsRoutingModule,
@@ -19,6 +30,8 @@ import { NgModule } from '@angular/core';
     StakingModule,
     DistributionModule,
     TxsModule,
+    YieldaggregatorModule,
+    AddressModule,
   ],
 })
 export class AppAccountsModule {}

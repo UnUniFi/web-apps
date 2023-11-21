dabdd4a4223b9aa0077e0b0811d1a8d2a690f40c
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-11-21 23:34:36 +0900

    feat: pool detail

--- projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
+++ projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PoolComponent implements OnInit {
   description =
     'Similar to the fixed yield of stETH in Pendle. \n\nThree ways to deposit \n- Fixed Yield Tranche of stATOM strategy \n- Leveraged Variable Yield Tranche of stATOM strategy \n- IRS Liquidity Pool of stATOM strategy';
+  tab: 'deposit' | 'withdraw' = 'deposit';
 
   constructor() {}
 

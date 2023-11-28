883aefd309aa91f2d655e4eeb4071581b5e763b0
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-11-28 22:41:24 +0900

    feat: pool tx

--- projects/portal/src/app/pages/interest-rate-swap/pools/pool/pool.component.ts
+++ projects/portal/src/app/pages/interest-rate-swap/pools/pool/pool.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapRequest } from 'projects/portal/src/app/models/interest-rate-swap/interest-rate-swap.model';
 
 @Component({
   selector: 'app-pool',
   templateUrl: './pool.component.html',
-  styleUrls: ['./pool.component.css']
+  styleUrls: ['./pool.component.css'],
 })
 export class PoolComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onMintLP(data: SwapRequest) {}
+  onRedeemLP(data: SwapRequest) {}
 }
--- projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
+++ projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { SwapRequest } from 'projects/portal/src/app/models/interest-rate-swap/interest-rate-swap.model';
 
 @Component({
   selector: 'view-pool',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pool.component.css'],
 })
 export class PoolComponent implements OnInit {
+  inputUnderlying?: string;
+  inputLP?: string;
+  underlyingDenom? = 'uatom';
+  lpDenom = 'lp/';
+
   description = 'This Vault provides the fixed yield of stATOM.';
   tab: 'deposit' | 'withdraw' = 'deposit';
+
+  @Output()
+  appMintLP: EventEmitter<SwapRequest> = new EventEmitter<SwapRequest>();
+  @Output()
+  appRedeemLP: EventEmitter<SwapRequest> = new EventEmitter<SwapRequest>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onMintLP() {
+    if (!this.inputUnderlying) {
+      alert('Please input the token amount.');
+      return;
+    }
+    if (!this.underlyingDenom) {
+      alert('Please select the token.');
+      return;
+    }
+    this.appMintLP.emit({
+      readableAmount: this.inputUnderlying,
+      denom: this.underlyingDenom,
+    });
+  }
+
+  onRedeemLP() {
+    if (!this.inputLP) {
+      alert('Please input the LP token amount.');
+      return;
+    }
+    this.appRedeemLP.emit({
+      readableAmount: this.inputLP,
+      denom: this.lpDenom,
+    });
+  }
 }

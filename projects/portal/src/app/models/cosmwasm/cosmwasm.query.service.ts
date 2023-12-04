5d63d6bb56e2168a3e2abe7980544ca41a0909e8
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-12-04 22:14:05 +0900

    feat: wasm query

---
+++ projects/portal/src/app/models/cosmwasm/cosmwasm.query.service.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '../config.service';
+import { Uint128 } from './StrategyOsmosis.types';
+import { Injectable } from '@angular/core';
+import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CosmwasmQueryService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async executeWasmQuery(address: string, queryMsg: any) {
+    const config = await this.configService.config$.pipe(take(1)).toPromise();
+    const client = await CosmWasmClient.connect(config?.rpc || '');
+    const res = await client.queryContractSmart(address, queryMsg);
+    return res;
+  }
+
+  async getUnbonding(contractAddress: string, address: string): Promise<Uint128> {
+    const queryMsg = {
+      unbondings: { addr: address },
+    };
+    const res = await this.executeWasmQuery(contractAddress, queryMsg);
+    return res;
+  }
+}

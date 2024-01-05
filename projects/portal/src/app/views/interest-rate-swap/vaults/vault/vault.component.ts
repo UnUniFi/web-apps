fdba6a306ba39b66eae9fca8bc8699facdf0cd29
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-01-05 21:04:52 +0900

    feat: tranche detail

--- projects/portal/src/app/views/interest-rate-swap/vaults/vault/vault.component.ts
+++ projects/portal/src/app/views/interest-rate-swap/vaults/vault/vault.component.ts
@@ -107,6 +107,21 @@ export class VaultComponent implements OnInit {
     this.router.navigate(['interest-rate-swap', 'simple-vaults', this.contractAddress]);
   }
 
+  calcMaturity(pool: AllTranches200ResponseTranchesInner): number {
+    const maturity = Number(pool.maturity) + Number(pool.start_time);
+    return maturity;
+  }
+
+  calcRestDays(pool: AllTranches200ResponseTranchesInner): number {
+    const maturity = Number(pool.maturity) + Number(pool.start_time);
+    const diff = maturity * 1000 - Date.now();
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    return days;
+  }
+
   onMintPTYT(id: string) {
     if (!this.inputUnderlying) {
       alert('Please input the token amount.');
--- projects/portal/src/app/views/interest-rate-swap/vaults/vault/vault.module.ts
+++ projects/portal/src/app/views/interest-rate-swap/vaults/vault/vault.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { PipesModule } from 'projects/portal/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [VaultComponent],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, PipesModule],
   exports: [VaultComponent],
 })
 export class VaultModule {}

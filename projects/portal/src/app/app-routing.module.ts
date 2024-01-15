1400202a5c1d074b53bb50e339557cf765702391
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-01-15 17:42:44 +0900

    fix: routing scroll

--- projects/portal/src/app/app-routing.module.ts
+++ projects/portal/src/app/app-routing.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}

e4a4f0a2d41dfe2a67a30074700770bdffe4899d
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-01-18 15:16:32 +0900

    fix: maturity millisec

--- projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
+++ projects/portal/src/app/views/interest-rate-swap/pools/pool/pool.component.ts
@@ -115,7 +115,7 @@ export class PoolComponent implements OnInit {
 
   calcMaturity(pool: AllTranches200ResponseTranchesInner): number {
     const maturity = Number(pool.maturity) + Number(pool.start_time);
-    return maturity;
+    return maturity * 1000;
   }
 
   calcRestDays(pool: AllTranches200ResponseTranchesInner): number {

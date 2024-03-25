64e669d2539535c53155ad49d64a027bb858071c
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-03-25 17:24:21 +0900

    feat: config & GLU image

---
+++ projects/explorer/editions/ununifi/launch/gluon-beta/firebase-hosting/config.js
@@ -0,0 +1,231 @@
+const restPort = location.protocol === 'https:' ? 1318 : 1317;
+const websocketPort = location.protocol === 'https:' ? 26658 : 26657;
+const faucetUbtcPort = location.protocol === 'https:' ? 8001 : 8000;
+const faucetUguuPort = location.protocol === 'https:' ? 8003 : 8002;
+const faucetJpuPort = location.protocol === 'https:' ? 8005 : 8004;
+const faucetUethPort = location.protocol === 'https:' ? 8007 : 8006;
+const faucetEuuPort = location.protocol === 'https:' ? 8009 : 8008;
+const developerPort = location.protocol === 'https:' ? 3040 : 3030;
+
+const domainCauchyEA = 'gluon-beta.cauchye.net';
+
+const chainID = 'gluon-test-1';
+const chainName = 'Gluon (beta)';
+
+const bech32Prefix = {
+  accAddr: 'ununifi',
+  accPub: 'ununifipub',
+  valAddr: 'ununifivaloper',
+  valPub: 'ununifivaloperpub',
+  consAddr: 'ununifivalcons',
+  consPub: 'ununifivalconspub',
+};
+
+const messageModules = [
+  'bank',
+  'auth',
+  'crisis',
+  'distribution',
+  'evidence',
+  'genaccounts',
+  'gov',
+  'ibc',
+  'slashing',
+  'staking',
+  'auction',
+  'ununifidist',
+  'cdp',
+  'incentive',
+  'pricefeed',
+];
+
+const apps = [
+  { name: 'Utilities', link: '/', icon: 'assistant' },
+  { name: 'Interest Rate Swap', link: '/interest-rate-swap/simple-vaults', icon: 'table_chart' },
+];
+
+const denomMetadata = [
+  {
+    description: 'The governance token of Gluon.',
+    denom_units: [
+      {
+        denom: 'uguu',
+        exponent: 0,
+      },
+      {
+        denom: 'guu',
+        exponent: 6,
+      },
+    ],
+    base: 'uguu',
+    name: 'Gluon',
+    display: 'GLU',
+    symbol: 'GLU',
+  },
+  {
+    description: 'The governance token of OSMOSIS.',
+    denom_units: [
+      {
+        denom: 'uosmo',
+        exponent: 0,
+      },
+      {
+        denom: 'osmo',
+        exponent: 6,
+      },
+    ],
+    base: 'uosmo',
+    name: 'OSMOSIS',
+    display: 'OSMO',
+    symbol: 'OSMO',
+  },
+  {
+    description: 'The governance token of Cosmos Hub.',
+    denom_units: [
+      {
+        denom: 'uatom',
+        exponent: 0,
+      },
+      {
+        denom: 'atom',
+        exponent: 6,
+      },
+    ],
+    base: 'uatom',
+    name: 'COSMOS',
+    display: 'ATOM',
+    symbol: 'ATOM',
+  },
+  {
+    description: 'The first cryptocurrency invented in 2008',
+    denom_units: [
+      {
+        denom: 'ubtc',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'btc',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'ubtc',
+    display: 'BTC',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+  },
+  {
+    description: 'The currency of the U.S.A.',
+    denom_units: [
+      {
+        denom: 'uusd',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'usd',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'uusd',
+    display: 'USD',
+    name: 'US Dollar',
+    symbol: 'USD',
+  },
+  {
+    description: 'Stablecoin pegged to the USD',
+    denom_units: [
+      {
+        denom: 'uusdc',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'uusdc',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'uusdc',
+    display: 'USDC',
+    name: 'USD Coin',
+    symbol: 'USDC',
+  },
+  {
+    description: 'Derivatives Liquidity Provider Token',
+    denom_units: [
+      {
+        denom: 'udlp',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'dlp',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'udlp',
+    name: 'Liquidity Provider',
+    display: 'DLP',
+    symbol: 'DLP',
+  },
+];
+
+const configs = [
+  // CauchyE A node without Monitor
+  {
+    id: domainCauchyEA,
+    restURL: `${location.protocol}//${domainCauchyEA}:${restPort}`,
+    websocketURL: `${location.protocol.replace('http', 'ws')}//${domainCauchyEA}:${websocketPort}`,
+    chainID,
+    chainName,
+    bech32Prefix,
+    minimumGasPrices: [
+      {
+        denom: 'uguu',
+        amount: 0.015,
+      },
+    ],
+    apps,
+    denomMetadata,
+    extension: {
+      faucet: [
+        {
+          hasFaucet: true,
+          faucetURL: `${location.protocol}//${domainCauchyEA}:${faucetUbtcPort}`,
+          denom: 'ubtc',
+          creditAmount: 2000000, // amount to credit in max request
+          maxCredit: 2000000, // account has already maxCredit balance cannot claim anymore
+        },
+        {
+          hasFaucet: true,
+          faucetURL: `${location.protocol}//${domainCauchyEA}:${faucetUguuPort}`,
+          denom: 'uguu',
+          creditAmount: 2000000000,
+          maxCredit: 2000000000,
+        },
+        {
+          hasFaucet: true,
+          faucetURL: `${location.protocol}//${domainCauchyEA}:${faucetUguuPort}`,
+          denom: 'uusdc',
+          creditAmount: 2000000000,
+          maxCredit: 2000000000,
+        },
+      ],
+      monitor: undefined,
+      nftMint: {
+        enabled: true,
+        nftClasses: ['ununifi-1AFC3C85B52311F13161F724B284EF900458E3B3'],
+      },
+      developer: {
+        enabled: true,
+        developerURL: `${location.protocol}//${domainCauchyEA}:${developerPort}`,
+      },
+      navigations: [],
+      messageModules,
+    },
+  },
+];
--- projects/portal/src/app/models/cosmos/bank.query.service.ts
+++ projects/portal/src/app/models/cosmos/bank.query.service.ts
@@ -204,6 +204,10 @@ export class BankQueryService {
         image: 'assets/UnUniFi-logo.png',
       },
       {
+        symbol: 'GLU',
+        image: 'assets/favicon.png',
+      },
+      {
         symbol: 'BTC',
         image:
           'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/svg/color/btc.svg',
